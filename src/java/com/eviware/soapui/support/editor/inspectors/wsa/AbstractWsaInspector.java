42602e570eed46f318a6603e21e68a06f705e47b
Dragica Soldo <dragica.soldo@smartbear.com>
2008-08-19 14:21:54 +0000

    refactoring

--- src/java/com/eviware/soapui/support/editor/inspectors/wsa/AbstractWsaInspector.java
+++ src/java/com/eviware/soapui/support/editor/inspectors/wsa/AbstractWsaInspector.java
@@ -47,33 +47,13 @@ public abstract class AbstractWsaInspector extends AbstractXmlInspector
 		{
 			mainPanel = new JPanel( new BorderLayout() );
 			form = new SimpleBindingForm( new PresentationModel<AbstractHttpRequest<?>>( wsaContainer.getWsaConfig() ) );
-			buildContent();
+			buildContent(form);
 			mainPanel.add( new JScrollPane( form.getPanel() ), BorderLayout.CENTER );
 		}
 		return mainPanel;
 	}
 
-	private void buildContent()
-	{
-		form.addSpace( 5 );
-		form.appendCheckBox("wsaEnabled", "Enable WS-A addressing", "Enable/Disable WS-A addressing");
-		form.addSpace( 5 );
-		//add mustUnderstand drop down list
-		form.appendComboBox( "mustUnderstand", "Must understand", new String[] {MustUnderstandTypeConfig.NONE.toString(), 
-				MustUnderstandTypeConfig.TRUE.toString(), MustUnderstandTypeConfig.FALSE.toString()},
-			"The  property for controlling use of the mustUnderstand attribute" );
-		
-		form.appendComboBox( "version", "WS-A Version", new String[] {WsaVersionTypeConfig.X_200508.toString(), WsaVersionTypeConfig.X_200408.toString()},
-			"The  property for managing WS-A version" );
-		
-		form.appendTextField( "action", "Action", "The action related to a message, will be generated if left empty" );
-		form.appendTextField( "from", "From", "The source endpoint reference, will be generated if left empty" );
-		form.appendTextField( "replyTo", "Reply to", "The reply endpoint reference, will be generated if left empty" );
-		form.appendTextField( "faultTo", "Fault to", "The fault endpoint reference" );
-		form.appendTextField( "messageID", "MessageID", " The ID of a message that can be used to uniquely identify a message, will be generated if left empty" );
-		form.appendTextField( "to", "To", "The destination endpoint reference, will be generated if left empty" );
-		form.addSpace( 5 );
-	}
+	public void buildContent(SimpleBindingForm form){};
 
 	@Override
 	public boolean isEnabledFor(EditorView<XmlDocument> view)
