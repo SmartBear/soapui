e0ba80946622cd569be13f497b68f2baa7093563
Ole Lensmar <ole.lensmar@smartbear.com>
2008-10-28 09:13:19 +0000

    made scriptText a public property

--- src/java/com/eviware/soapui/impl/wsdl/teststeps/assertions/basic/GroovyScriptAssertion.java
+++ src/java/com/eviware/soapui/impl/wsdl/teststeps/assertions/basic/GroovyScriptAssertion.java
@@ -48,291 +48,304 @@ import java.awt.event.MouseEvent;
 
 /**
  * Assertion performed by a custom Grooy Script
- * 
+ *
  * @author ole.matzura
  */
 
 public class GroovyScriptAssertion extends WsdlMessageAssertion implements RequestAssertion, ResponseAssertion
 {
-	public static final String ID = "GroovyScriptAssertion";
-	public static final String LABEL = "Script Assertion";
-	private String scriptText;
-	private SoapUIScriptEngine scriptEngine;
-	private JDialog dialog;
-	private GroovyScriptAssertionPanel groovyScriptAssertionPanel;
+   public static final String ID = "GroovyScriptAssertion";
+   public static final String LABEL = "Script Assertion";
+   private String scriptText;
+   private SoapUIScriptEngine scriptEngine;
+   private JDialog dialog;
+   private GroovyScriptAssertionPanel groovyScriptAssertionPanel;
 
-	public GroovyScriptAssertion( TestAssertionConfig assertionConfig, Assertable modelItem )
-	{
-		super( assertionConfig, modelItem, true, true, true, false );
-		
-		XmlObjectConfigurationReader reader = new XmlObjectConfigurationReader( getConfiguration());
-		scriptText = reader.readString( "scriptText", "" );
-      
-		scriptEngine = SoapUIScriptEngineRegistry.create( SoapUIScriptEngineRegistry.GROOVY_ID, this );
-		scriptEngine.setScript( scriptText );
-	}
+   public GroovyScriptAssertion( TestAssertionConfig assertionConfig, Assertable modelItem )
+   {
+      super( assertionConfig, modelItem, true, true, true, false );
 
-	@Override
-	protected String internalAssertRequest( MessageExchange messageExchange, SubmitContext context ) throws AssertionException
-	{
-		return assertScript( messageExchange, context, SoapUI.ensureGroovyLog() );
-	}
+      XmlObjectConfigurationReader reader = new XmlObjectConfigurationReader( getConfiguration() );
+      scriptText = reader.readString( "scriptText", "" );
 
-	private String assertScript( MessageExchange messageExchange, SubmitContext context, Logger log ) throws AssertionException
-	{
-		try
-		{
-			scriptEngine.setVariable("context", context);
-			scriptEngine.setVariable("messageExchange", messageExchange);
-			scriptEngine.setVariable( "log", log );
-			
-			Object result = scriptEngine.run();
-			return result == null ? null : result.toString();
-		}
-		catch( Throwable e )
-		{
-			throw new AssertionException( new AssertionError( e.getMessage() ));
-		}
-		finally
-		{
-			scriptEngine.clearVariables();
-		}
-	}
+      scriptEngine = SoapUIScriptEngineRegistry.create( SoapUIScriptEngineRegistry.GROOVY_ID, this );
+      scriptEngine.setScript( scriptText );
+   }
 
-	@Override
-	protected String internalAssertResponse( MessageExchange messageExchange, SubmitContext context ) throws AssertionException
-	{
-		return assertScript( messageExchange, context, SoapUI.ensureGroovyLog() );
-	}
+   @Override
+   protected String internalAssertRequest( MessageExchange messageExchange, SubmitContext context ) throws AssertionException
+   {
+      return assertScript( messageExchange, context, SoapUI.ensureGroovyLog() );
+   }
 
-	@Override
-	public boolean configure()
-	{
-		if( dialog == null )
-		{
-			dialog = new JDialog( UISupport.getMainFrame(), "Script Assertion", true );
-			groovyScriptAssertionPanel = new GroovyScriptAssertionPanel();
-			dialog.setContentPane( groovyScriptAssertionPanel );
-			UISupport.initDialogActions( dialog, groovyScriptAssertionPanel.getShowOnlineHelpAction(),
-						groovyScriptAssertionPanel.getDefaultButton());
-			dialog.setSize( 600, 500 );
-			dialog.setModal( true );
-			dialog.pack();
-		}
+   private String assertScript( MessageExchange messageExchange, SubmitContext context, Logger log ) throws AssertionException
+   {
+      try
+      {
+         scriptEngine.setVariable( "context", context );
+         scriptEngine.setVariable( "messageExchange", messageExchange );
+         scriptEngine.setVariable( "log", log );
 
-		UISupport.showDialog( dialog );
-		
-		setConfiguration( createConfiguration() );
-		return true;
-	}
+         Object result = scriptEngine.run();
+         return result == null ? null : result.toString();
+      }
+      catch( Throwable e )
+      {
+         throw new AssertionException( new AssertionError( e.getMessage() ) );
+      }
+      finally
+      {
+         scriptEngine.clearVariables();
+      }
+   }
 
-	protected XmlObject createConfiguration()
-	{
-		XmlObjectConfigurationBuilder builder = new XmlObjectConfigurationBuilder();
-		builder.add( "scriptText", scriptText );
-		return builder.finish();
-	}
+   @Override
+   protected String internalAssertResponse( MessageExchange messageExchange, SubmitContext context ) throws AssertionException
+   {
+      return assertScript( messageExchange, context, SoapUI.ensureGroovyLog() );
+   }
 
-	private class GroovyScriptAssertionPanel extends JPanel
-	{
-		private GroovyEditor editor;
-		private JSplitPane mainSplit;
-		private JLogList logArea;
-		private RunAction runAction = new RunAction();
-		private Logger logger;
-		private JButton okButton;
-		private ShowOnlineHelpAction showOnlineHelpAction;
+   @Override
+   public boolean configure()
+   {
+      if( dialog == null )
+      {
+         dialog = new JDialog( UISupport.getMainFrame(), "Script Assertion", true );
+         groovyScriptAssertionPanel = new GroovyScriptAssertionPanel();
+         dialog.setContentPane( groovyScriptAssertionPanel );
+         UISupport.initDialogActions( dialog, groovyScriptAssertionPanel.getShowOnlineHelpAction(),
+                 groovyScriptAssertionPanel.getDefaultButton() );
+         dialog.setSize( 600, 500 );
+         dialog.setModal( true );
+         dialog.pack();
+      }
 
-		public GroovyScriptAssertionPanel()
-		{
-			super( new BorderLayout() );
-			
-			buildUI();
-			setPreferredSize( new Dimension( 600, 440 ));
-			
-			logger = Logger.getLogger( "ScriptAssertion." + getName() );
-			editor.requestFocusInWindow();
-		}
+      UISupport.showDialog( dialog );
 
-		public void release()
-		{
-			logArea.release();
-			editor.release();
-			logger = null;
-		}
-		
-		private void buildUI()
-		{
-			editor = new GroovyEditor( new ScriptStepGroovyEditorModel( ));
-			
-			logArea = new JLogList( "Groovy Test Log" );
-			logArea.addLogger( "ScriptAssertion." + getName(), true );
-			logArea.getLogList().addMouseListener( new MouseAdapter() {
+      setConfiguration( createConfiguration() );
+      return true;
+   }
 
-				public void mouseClicked(MouseEvent e)
-				{
-					if( e.getClickCount() < 2 )
-						return;
-					
-					String value = logArea.getLogList().getSelectedValue().toString();
-					if( value == null )
-						return;
-					
-					editor.selectError( value );
-				}} );
-			
-			editor.setBorder( BorderFactory.createCompoundBorder( BorderFactory.createEmptyBorder( 0, 3, 0, 3 ), 
-						editor.getBorder() ));
-			
-			mainSplit = UISupport.createVerticalSplit( editor, logArea);
-			mainSplit.setDividerLocation( 280 );
-			mainSplit.setResizeWeight( 0.8 );
-			add( mainSplit, BorderLayout.CENTER );
-			add( buildToolbar(), BorderLayout.NORTH );
-			add( buildStatusBar(), BorderLayout.SOUTH );
-		}
-		
-		public JButton getDefaultButton()
-		{
-			return okButton;
-		}
-		
-		public ShowOnlineHelpAction getShowOnlineHelpAction()
-		{
-			return showOnlineHelpAction;
-		}
+   protected XmlObject createConfiguration()
+   {
+      XmlObjectConfigurationBuilder builder = new XmlObjectConfigurationBuilder();
+      builder.add( "scriptText", scriptText );
+      return builder.finish();
+   }
 
-		private Component buildStatusBar()
-		{
-			ButtonBarBuilder builder = new ButtonBarBuilder();
-			
-			showOnlineHelpAction = new ShowOnlineHelpAction( HelpUrls.GROOVYASSERTION_HELP_URL );
-			builder.addFixed( UISupport.createToolbarButton( showOnlineHelpAction));
-			builder.addGlue();
-			okButton = new JButton( new OkAction() );
-			builder.addFixed( okButton );
-			builder.setBorder( BorderFactory.createEmptyBorder( 0, 3, 3, 3 ));
-			return builder.getPanel();
-		}
+   public String getScriptText()
+   {
+      return scriptText;
+   }
 
-		private JComponent buildToolbar()
-		{
-			JXToolBar toolBar = UISupport.createToolbar();
-			JButton runButton = UISupport.createToolbarButton( runAction );
-			toolBar.add( runButton );
-			toolBar.add( Box.createHorizontalGlue() );
-			JLabel label = new JLabel("<html>Script is invoked with <code>log</code>, <code>context</code> " +
-							"and <code>messageExchange</code> variables</html>");
-			label.setToolTipText( label.getText() );
-			label.setMaximumSize( label.getPreferredSize() );
-			
-			toolBar.addFixed( label);
-			toolBar.addSpace( 3 );
-			
-			return toolBar;
-		}
+   public void setScriptText( String scriptText )
+   {
+      this.scriptText = scriptText;
+      scriptEngine.setScript( scriptText );
+   }
 
-		private final class OkAction extends AbstractAction
-		{
-			public OkAction()
-			{
-			   super( "OK" );	
-			}
-			
-			public void actionPerformed( ActionEvent e )
-			{
-				dialog.setVisible( false );
-			}
-		}
+   private class GroovyScriptAssertionPanel extends JPanel
+   {
+      private GroovyEditor editor;
+      private JSplitPane mainSplit;
+      private JLogList logArea;
+      private RunAction runAction = new RunAction();
+      private Logger logger;
+      private JButton okButton;
+      private ShowOnlineHelpAction showOnlineHelpAction;
 
-		private class ScriptStepGroovyEditorModel implements GroovyEditorModel
-		{
-			public String[] getKeywords()
-			{
-				return new String[] {"log", "context", "messageExchange"};
-			}
+      public GroovyScriptAssertionPanel()
+      {
+         super( new BorderLayout() );
 
-			public Action getRunAction()
-			{
-				return runAction;
-			}
+         buildUI();
+         setPreferredSize( new Dimension( 600, 440 ) );
 
-			public String getScript()
-			{
-				return scriptText;
-			}
+         logger = Logger.getLogger( "ScriptAssertion." + getName() );
+         editor.requestFocusInWindow();
+      }
 
-			public void setScript( String text )
-			{
-				scriptText = text;
-				scriptEngine.setScript( scriptText );
-			}
+      public void release()
+      {
+         logArea.release();
+         editor.release();
+         logger = null;
+      }
 
-			public Settings getSettings()
-			{
-				return SoapUI.getSettings();
-			}
+      private void buildUI()
+      {
+         editor = new GroovyEditor( new ScriptStepGroovyEditorModel() );
 
-			public String getScriptName()
-			{
-				return "Assertion";
-			}}
-		
-		private class RunAction extends AbstractAction
-		{
-			public RunAction()
-			{
-				putValue( Action.SMALL_ICON, UISupport.createImageIcon( "/run_groovy_script.gif" ));
-				putValue( Action.SHORT_DESCRIPTION, "Runs this assertion script against the last messageExchange with a mock testContext" );
-			}
+         logArea = new JLogList( "Groovy Test Log" );
+         logArea.addLogger( "ScriptAssertion." + getName(), true );
+         logArea.getLogList().addMouseListener( new MouseAdapter()
+         {
 
-			public void actionPerformed(ActionEvent e)
-			{
-				TestStep testStep = ( TestStep ) getAssertable().getModelItem();
-				WsdlMessageExchange exchange = null;
-				
-				if( testStep instanceof WsdlTestRequestStep )
-				{
-					WsdlTestRequestStep testRequestStep = ( WsdlTestRequestStep ) testStep;
-					exchange = new WsdlResponseMessageExchange( testRequestStep.getTestRequest() );
-					((WsdlResponseMessageExchange)exchange).setResponse( testRequestStep.getTestRequest().getResponse() );
-				}
-				
-				try
-				{
-					String result = assertScript( exchange, new WsdlTestRunContext( testStep ), logger );
-					UISupport.showInfoMessage( "Script Assertion Passed" + ((result==null) ? "" : ": [" + result + "]" ));
-				}
-				catch( AssertionException e1 )
-				{
-					UISupport.showErrorMessage( e1.getMessage() );
-				}
-				catch( Throwable t )
-				{
-					SoapUI.logError( t );
-					UISupport.showErrorMessage( t.getMessage() );
-				}
-				
-				editor.requestFocusInWindow();
-			}
-		}
-	}
+            public void mouseClicked( MouseEvent e )
+            {
+               if( e.getClickCount() < 2 )
+                  return;
 
-	@Override
-	public void release()
-	{
-		super.release();
-		scriptEngine.release();
-		
-		if( groovyScriptAssertionPanel != null )
-			groovyScriptAssertionPanel.release();
-	}
-	
-	public static class Factory extends AbstractTestAssertionFactory
-	{
-		public Factory()
-		{
-			super(GroovyScriptAssertion.ID, GroovyScriptAssertion.LABEL, GroovyScriptAssertion.class);
-		}
-	}
+               String value = logArea.getLogList().getSelectedValue().toString();
+               if( value == null )
+                  return;
+
+               editor.selectError( value );
+            }
+         } );
+
+         editor.setBorder( BorderFactory.createCompoundBorder( BorderFactory.createEmptyBorder( 0, 3, 0, 3 ),
+                 editor.getBorder() ) );
+
+         mainSplit = UISupport.createVerticalSplit( editor, logArea );
+         mainSplit.setDividerLocation( 280 );
+         mainSplit.setResizeWeight( 0.8 );
+         add( mainSplit, BorderLayout.CENTER );
+         add( buildToolbar(), BorderLayout.NORTH );
+         add( buildStatusBar(), BorderLayout.SOUTH );
+      }
+
+      public JButton getDefaultButton()
+      {
+         return okButton;
+      }
+
+      public ShowOnlineHelpAction getShowOnlineHelpAction()
+      {
+         return showOnlineHelpAction;
+      }
+
+      private Component buildStatusBar()
+      {
+         ButtonBarBuilder builder = new ButtonBarBuilder();
+
+         showOnlineHelpAction = new ShowOnlineHelpAction( HelpUrls.GROOVYASSERTION_HELP_URL );
+         builder.addFixed( UISupport.createToolbarButton( showOnlineHelpAction ) );
+         builder.addGlue();
+         okButton = new JButton( new OkAction() );
+         builder.addFixed( okButton );
+         builder.setBorder( BorderFactory.createEmptyBorder( 0, 3, 3, 3 ) );
+         return builder.getPanel();
+      }
+
+      private JComponent buildToolbar()
+      {
+         JXToolBar toolBar = UISupport.createToolbar();
+         JButton runButton = UISupport.createToolbarButton( runAction );
+         toolBar.add( runButton );
+         toolBar.add( Box.createHorizontalGlue() );
+         JLabel label = new JLabel( "<html>Script is invoked with <code>log</code>, <code>context</code> " +
+                 "and <code>messageExchange</code> variables</html>" );
+         label.setToolTipText( label.getText() );
+         label.setMaximumSize( label.getPreferredSize() );
+
+         toolBar.addFixed( label );
+         toolBar.addSpace( 3 );
+
+         return toolBar;
+      }
+
+      private final class OkAction extends AbstractAction
+      {
+         public OkAction()
+         {
+            super( "OK" );
+         }
+
+         public void actionPerformed( ActionEvent e )
+         {
+            dialog.setVisible( false );
+         }
+      }
+
+      private class ScriptStepGroovyEditorModel implements GroovyEditorModel
+      {
+         public String[] getKeywords()
+         {
+            return new String[]{"log", "context", "messageExchange"};
+         }
+
+         public Action getRunAction()
+         {
+            return runAction;
+         }
+
+         public String getScript()
+         {
+            return getScriptText();
+         }
+
+         public void setScript( String text )
+         {
+            setScriptText( text );
+         }
+
+         public Settings getSettings()
+         {
+            return SoapUI.getSettings();
+         }
+
+         public String getScriptName()
+         {
+            return "Assertion";
+         }
+      }
+
+      private class RunAction extends AbstractAction
+      {
+         public RunAction()
+         {
+            putValue( Action.SMALL_ICON, UISupport.createImageIcon( "/run_groovy_script.gif" ) );
+            putValue( Action.SHORT_DESCRIPTION, "Runs this assertion script against the last messageExchange with a mock testContext" );
+         }
+
+         public void actionPerformed( ActionEvent e )
+         {
+            TestStep testStep = (TestStep) getAssertable().getModelItem();
+            WsdlMessageExchange exchange = null;
+
+            if( testStep instanceof WsdlTestRequestStep )
+            {
+               WsdlTestRequestStep testRequestStep = (WsdlTestRequestStep) testStep;
+               exchange = new WsdlResponseMessageExchange( testRequestStep.getTestRequest() );
+               ( (WsdlResponseMessageExchange) exchange ).setResponse( testRequestStep.getTestRequest().getResponse() );
+            }
+
+            try
+            {
+               String result = assertScript( exchange, new WsdlTestRunContext( testStep ), logger );
+               UISupport.showInfoMessage( "Script Assertion Passed" + ( ( result == null ) ? "" : ": [" + result + "]" ) );
+            }
+            catch( AssertionException e1 )
+            {
+               UISupport.showErrorMessage( e1.getMessage() );
+            }
+            catch( Throwable t )
+            {
+               SoapUI.logError( t );
+               UISupport.showErrorMessage( t.getMessage() );
+            }
+
+            editor.requestFocusInWindow();
+         }
+      }
+   }
+
+   @Override
+   public void release()
+   {
+      super.release();
+      scriptEngine.release();
+
+      if( groovyScriptAssertionPanel != null )
+         groovyScriptAssertionPanel.release();
+   }
+
+   public static class Factory extends AbstractTestAssertionFactory
+   {
+      public Factory()
+      {
+         super( GroovyScriptAssertion.ID, GroovyScriptAssertion.LABEL, GroovyScriptAssertion.class );
+      }
+   }
 }
