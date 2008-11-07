70c303db68c4ea21996f6879d696c9413da2f9b3
Ole Lensmar <ole.lensmar@smartbear.com>
2008-11-07 13:19:43 +0000

    added appindex

---
+++ xdocs/appindex/soapUIPro_start.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+-->
+</style>
+<link href="startpage.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+-->
+</style>
+<script type="text/javascript">_uacct = "UA-92447-1";
+urchinTracker();</script>
+</head>
+
+
+<body>
+
+<table width="700" height="825" border="0" cellpadding="0">
+  
+  <tr>
+    <td height="75" colspan="3"><img src="Images/soapuipro_header.png" alt="soapUI Header" width="685" height="68" longdesc="http://www.soapui.org" /></td>
+  </tr>
+  <tr>
+    <td height="720" width="150" valign="top">
+    <table height="720" width="150" border="0" cellpadding="5">
+      <tr>
+        <td height="240" valign="top" style="border:solid thin #CCCCCC"> <h2>soapUI Training</h2>
+          <p>Become a soapUI Pro! PushToTest deliver soapUI training world wide this autumn.<br />
+              <br />
+              <strong>Locations:</strong><br />
+            - Silicon Valley<br />
+            - London<br />
+            - New York<br />
+  <br />
+  <a onclick="javascript:urchinTracker('/appindexsoapUIProTraining')" href="http://www.eviware.com/content/view/121/16/" title="Read more about soapUI training" target="_blank">Training Information</a></p></td>
+      </tr>
+      <tr>
+        <td height="240" class="greyBox" valign="top"><h2>eviware I turn</h2>
+          <p>Ole Matzura presents in his blog Tips &amp; Tricks for your soapUI usage.</p>
+          <p>Learn all the latest ins and outs of the Tool at eviware.com.<br />
+            <br />
+            <a onclick="javascript:urchinTracker('/appindexsoapUIProOleBlog')"href="http://www.eviware.com/blogs/oleblog" title="Read it Here">Read it Here</a></p>
+          </td>
+      </tr>
+      <tr>
+        <td height="240" class="greyBox" valign="top"><h2>What's next?</h2>
+          <p>Are there important features you feel are missing in soapUI Pro?</p>
+          <p>&nbsp;</p>
+          <p>Please don't be afraid of telling us what's missing.<br />
+            <br />
+          <a onclick="javascript:urchinTracker('/appindexsoapUIProForum')" href="http://www.eviware.com/component/option,com_smf/Itemid,53/" title="Visit our forums" target="_blank">Go to the Forums</a></p></td>
+      </tr>
+    </table>      <p>&nbsp;</p>    </td>
+    <td height="720" width="400" class="greyBox" valign="top"><h1><img src="Images/Pro-Icon-50.png" alt="soapUI" width="50" height="50" /> Latest News</h1>
+      <p class="center_p"><em><strong>September 26 2008- SoapUI 2.5: The REST Release beta 1 is out.</strong></em></p>
+      <p class="center_p">soapUI is now <strong>restUI</strong> as well! <br />
+        <br />
+      In the new release you get full support for REST Testing. We would like to think it's a gem! soapUI REST Testing makes it possible for you to do complete RestTests with or without a WADL.<br />
+
+      <p align="center" class="center_p"><a onclick="javascript:urchinTracker('/appindexsoapUIProReadMore')" href="http://www.eviware.com/new.htm" title="Read more about soapUI 2.1" target="_blank">Read more here</a> | <a onclick="javascript:urchinTracker('/appindexsoapUIProDownloadNewest')" href="http://www.eviware.com/download/" title="download soapUI" target="_blank">Download it here</a></p>
+ <hr/>
+<h2>Latest news and posts:</h2><p>
+- October 7 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=52">REST Resources, Parameters, etc</a><br/>
+- October 5 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=264">Using XPath Assertions in soapUI</a><br/>
+- October 5 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=208">The new DataGen TestStep</a><br/>
+- October 4 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=217">Send email from your groovy scripts</a><br/>
+- October 1 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=194">DataDriven Load Testing </a><br/>
+- October 1 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=17">Improving soapUI Memory Usage</a><br/>
+- September 30 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=74">Test your feeds with soapUI</a><br/>
+- September 30 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=11">Testing REST Services with soapUI</a><br/>
+- September 23 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=6">Dynamic Property-Expansions...</a><br/>
+</p>      <hr/>
+      <h1><img src="Images/Pro-Icon-50.png" alt="soapUI" width="50" height="50" /> Vote soapUI!</h1>
+      <p class="center_p"><strong>Vote for soapUI in SOA World Magazine Reader's Choice Vote.</strong></p>
+      <table width="381" border="0" cellpadding="0">
+        <tr>
+          <td width="125"><img src="images/WSJ-SOAReadersChoice-125_125px.jpg" width="125" height="125" /></td>
+          <td width="250" class="center_p">            We are participating in several categories and need your support.<br /> 
+              <span class="GreyText">If </span>you use soapUI and enjoy it; why not cast your vote on soapUI? <span class="GreyText">Thanks </span>for your support, we appreciate it!<br />
+              <br />
+          <div align="center"><a onclick="javascript:urchinTracker('/appindexsoapUIProVote')" href="http://soa.sys-con.com/general/readerschoice.htm"> Vote Here! </a></div></td>
+        </tr>
+      </table>
+      <hr/>
+      <h1><img src="Images/Pro-Icon-50.png" alt="soapUI" width="50" height="50" /> How do I start?</h1>
+      <p class="center_p"><em><strong>New soapUI User?</strong></em></p>
+    <p class="center_p"><em> Start by doing the following:</em></p>
+    <ul class="evilist">
+      <li>						Read the <a onclick="javascript:urchinTracker('/appindexsoapUIProFeatures')" href="http://www.soapui.org/features.html">Feature Overview</a><br />
+      </li>
+      <li>Try your hands on <a onclick="javascript:urchinTracker('/appindexsoapUIProSampleProj');"href="http://www.soapui.org/gettingstarted/sample-project.html">the Example soapUI Project</a>        </li>
+      <li>Read the <a onclick="javascript:urchinTracker('/appindexsoapUIProgettingSTarted');"href="http://www.soapui.org/gettingstarted/index.html">Getting Started with soapUI Guide</a>        </li>
+      <li>Watch the <a onclick="javascript:urchinTracker('/appindexsoapUIProMovies');"href="http://www.soapui.org/movies/sample_project/sample_project.html">Example Project Movie</a></li>
+    </ul>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+    <td height="720" width="150" valign="top" class="greyBox"><h2>What's new in soapUI 2.5?</h2>
+      <p class="RighColumnList">- Rest Testing<br />
+        - HTTP Testing<br />
+        - WADL Generation<br />
+        - WADL Documentation<br />
+        - Java Code from REST<br />
+        - JSON Viewer<br />
+        - REST TestStep<br />
+        - WS-Addressing<br />
+        - Encrypted Project 
+&nbsp;&nbsp;Files<br />
+        - Relative Projects<br />
+        - Script LoadTest <br />
+        &nbsp;&nbsp;&nbsp;Strategy <br />
+        - Grid <br />
+         &nbsp;&nbsp;LoadTest Strategy<br />
+        - Scripting before and <br />
+        &nbsp;&nbsp;after LoadTests<br />
+        - Improved HTTP &nbsp;&nbsp;Monitor<br />
+        - Improved <br />
+        &nbsp;&nbsp;MockService<br />
+        - Inline Scripting<span class="RighColumnList"><br />
+        - 
+        DatGen TestStep<br />
+        - Composite Project Files<br />
+- DataDriven <br />
+&nbsp;&nbsp;LoadTests<br />
+        - Coverage Reports <br />
+&nbsp;&nbsp;        from Command line</span><br />
+<br />
+and much more...<br />
+</p>
+      <p class="RighColumnList">We have in depth information available at the soapui.org web site</p>
+      <p class="RighColumnList"><br />
+        <a onclick="javascript:urchinTracker('/appindexsoapUIProReadMoreRight')" href="http://www.soapui.org/new_and_noteworthy_2_5.html" title="Read more about soapUI 2.1" target="_blank">Read more here</a>
+        </li>
+      </p>
+      <p>&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ xdocs/appindex/soapUI_start.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+-->
+</style>
+<link href="startpage.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+-->
+</style>
+<script type="text/javascript">_uacct = "UA-92447-1";
+urchinTracker();</script>
+</head>
+
+<body>
+
+
+
+
+<table width="700" height="825" border="0" cellpadding="0">
+  
+  <tr>
+    <td height="75" colspan="3"><img src="Images/soapui_header.png" alt="soapUI Header" width="685" height="68" longdesc="http://www.soapui.org" /></td>
+  </tr>
+  <tr>
+    <td height="720" width="150" valign="top" cellpadding="0">
+    <table height="720" width="150" border="0" cellpadding="5">
+      <tr>
+        <td height="240" valign="top"><h2>The benefits of going Pro</h2>
+          <p align="left">As you might know, soapUI is also available in a Pro version.<br />
+            <br />
+            Let us explain the <em><strong>advantages</strong></em> and show you the <em><strong>extra features </strong></em>you get.<br />
+            <br />
+            <a onclick="javascript:urchinTracker('/appindexsoapUIReadMorePro')" href="http://www.eviware.com/new.htm" title="Read more about soapUI 2.1" target="_blank">Read more here</a></p>          </td>
+      </tr>
+      <tr>
+        <td height="240" class="greyBox" valign="top"><h2>eviware I turn</h2>
+          <p>Ole Matzura presents in his blog Tips &amp; Tricks for your soapUI usage.</p>
+          <p>Learn all the latest ins and outs of the Tool at eviware.com.<br />
+              <br />
+            <a onclick="javascript:urchinTracker('/appindexsoapUIProOleBlog')"href="http://www.eviware.com/blogs/oleblog" title="Read it Here">Read it Here</a></p></td>
+      </tr>
+      <tr>
+        <td height="240"  class="greyBox" valign="top"><h2>soapUI Training</h2>
+          <p>Our partner PushToTest deliver soapUI training world wide this autumn.<br />
+              <br />
+              <strong>Places:</strong><br />
+              - Silicon Valley<br />
+              - London<br />
+              - New York<br />
+            <br />
+            <a onclick="javascript:urchinTracker('/appindexsoapUITraining')" href="http://www.eviware.com/content/view/121/16/" title="Read more about soapUI training" target="_blank">Training Information</a></p></td>
+      </tr>
+    </table>      <p>&nbsp;</p>    </td>
+    <td height="720" width="400" valign="top" class="greyBox"><h1><img src="Images/soapui-logo.png" alt="soapUI" width="50" height="50" /> Latest News</h1>
+      <p class="center_p"><em><strong>September 26 2008- SoapUI 2.5: The REST Release beta 1 is out.</strong></em></p>
+      <p class="center_p">soapUI is now <strong>restUI</strong> as well! <br />
+        <br />
+      In the new release you get full support for REST Testing. We would like to think it's a gem! soapUI REST Testing makes it possible for you to do complete RestTests with or without a WADL.<br />
+      <p align="center" class="center_p"><a onclick="javascript:urchinTracker('/appindexsoapUIReadMore')" href="http://www.eviware.com/new.htm" title="Read more about soapUI 2.1" target="_blank">Read more here</a> | <a onclick="javascript:urchinTracker('/appindexsoapUIDownloadNewest')" href="http://www.sourceforge.net" title="download soapUI" target="_blank">Download it here</a>|</p>
+      <hr/>
+	  
+<h1>Latest news and posts:</h1>
+<p>
+- October 7 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=52">REST Resources, Parameters, etc</a><br/>
+- October 5 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=264">Using XPath Assertions in soapUI</a><br/>
+- October 4 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=217">Send email from your groovy scripts</a><br/>
+- October 1 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=17">Improving soapUI Memory Usage</a><br/>
+- October 1 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=126">Building and running from SVN</a><br/>
+- October 1 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=110">Deploying MockServices as Servlets</a><br/>
+- September 30 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=74">Test your feeds with soapUI</a><br/>
+- September 30 2008 : <a href="http://www.eviware.com/blogs/oleblog/?p=11">Testing REST Services with soapUI</a><br/>
+</p>
+      <hr/>
+      <h1><img src="Images/soapui-logo.png" alt="soapUI" width="50" height="50" /> Vote soapUI!</h1>
+      <p class="center_p"><strong>Vote for soapUI in SOA World Magazine Reader's Choice Vote.</strong></p>
+      <table width="381" border="0" cellpadding="0">
+        <tr>
+          <td width="125"><img src="images/WSJ-SOAReadersChoice-125_125px.jpg" width="125" height="125" /></td>
+          <td width="250" class="center_p">            We are participating in several categories and need your support.<br /> 
+              <span class="GreyText">If </span>you use soapUI and enjoy it; why not cast your vote on soapUI? <span class="GreyText">Thanks </span>for your support, we appreciate it!<br />
+              <br />
+          <div align="center"><a onclick="javascript:urchinTracker('/appindexsoapUIVote')" href="http://soa.sys-con.com/general/readerschoice.htm"> Vote Here! </a></div></td>
+        </tr>
+      </table>
+      <hr/>
+      <h1><img src="Images/soapui-logo.png" alt="soapUI" width="50" height="50" /> How do I start?</h1>
+      <p class="center_p"><em><strong>New soapUI User?</strong></em></p>
+    <p class="center_p"><em> Start by doing the following:</em></p>
+    <ul class="evilist">
+      <li>Read the <a onclick="javascript:urchinTracker('/appindexsoapUIFeatures')" href="http://www.soapui.org/features.html">Feature Overview</a>        </li>
+      <li>Read the <a onclick="javascript:urchinTracker('/appindexsoapUIProComparison')" href="http://www.soapui.org/comparison.html">soapUI Pro comparison</a><br />
+      </li>
+      <li>Try your hands on <a onclick="javascript:urchinTracker('/appindexsoapUISampleProj');"href="http://www.soapui.org/gettingstarted/sample-project.html">the Example soapUI Project</a>        </li>
+      <li>Read the <a onclick="javascript:urchinTracker('/appindexsoapUIgettingSTarted');"href="http://www.soapui.org/gettingstarted/index.html">Getting Started with soapUI Guide</a>        </li>
+      <li>Watch the <a onclick="javascript:urchinTracker('/appindexsoapUIMovies');"href="http://www.soapui.org/movies/sample_project/sample_project.html">Example Project Movie</a></li>
+    </ul>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+    <td height="720" width="150" valign="top" class="greyBox" ><h2>What's new in soapUI 2.5?</h2>
+      <p class="RighColumnList">- Rest Testing<br />
+        - HTTP Testing<br />
+        - WADL Generation<br />
+        - WADL Documentation<br />
+        - Java Code from REST<br />
+        - JSON Viewer<br />
+        - REST TestStep<br />
+        - WS-Addressing<br />
+        - Encrypted Project 
+&nbsp;&nbsp;Files<br />
+        - Relative Projects<br />
+        - Script LoadTest <br />
+        &nbsp;&nbsp;&nbsp;Strategy <br />
+        - Grid <br />
+         &nbsp;&nbsp;LoadTest Strategy<br />
+        - Scripting before and <br />
+        &nbsp;&nbsp;after LoadTests<br />
+        - Improved HTTP &nbsp;&nbsp;Monitor<br />
+        - Improved <br />
+        &nbsp;&nbsp;MockService<br />
+        - Inline Scripting<span class="RighColumnList"><br />
+ 
+      <p><span class="narrowcolumnheading">soapUI Pro users also get:</span><br />
+        <span class="RighColumnList">- The Pro Functionality for REST Testing<br />
+        - DatGen TestStep<br />
+        - Composite Project Files<br />
+- DataDriven <br />
+&nbsp;&nbsp;LoadTests<br />
+        - Coverage Reports <br />
+&nbsp;&nbsp;        from Command line</span><br />
+<br />
+and much more...<br />
+</p>
+      <p class="RighColumnList">We have in depth information available at the soapui.org web site</p>
+      <p class="RighColumnList"><br />
+        <a onclick="javascript:urchinTracker('/appindexsoapUIProReadMoreRight')" href="http://www.soapui.org/new_and_noteworthy_2_5.html" title="Read more about soapUI 2.1" target="_blank">Read more here</a>
+        </li>
+      </p>
+    <p>&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
