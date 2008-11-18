6701725176618afed07bd8b50a6c9d6bad3a10a8
Dragica Soldo <dragica.soldo@smartbear.com>
2008-11-18 22:01:27 +0000

    help urls

--- src/java/com/eviware/soapui/impl/wsdl/support/HelpUrls.java
+++ src/java/com/eviware/soapui/impl/wsdl/support/HelpUrls.java
@@ -137,7 +137,7 @@ public interface HelpUrls
 
 	public static final String CLONETESTSTEP_HELP_URL = HELP_URL_ROOT +"functional/testcases.html#Clone_TestSteps";
 
-	public static final String CLONETESTSUITE_HELP_URL = HELP_URL_ROOT +"functional/testsuites.html#Clone_TestSuite";
+	public static final String CLONETESTSUITE_HELP_URL = HELP_URL_ROOT +"functional/testsuites.html";
 
 	public static final String CHANGEOPERATION_HELP_URL = HELP_URL_ROOT +"functional/testrequests.html#Change_Operation";
 
