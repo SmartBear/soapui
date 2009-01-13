fed13baed25cc1558c3dc11d74257ab386428bb1
Ole Lensmar <ole.lensmar@smartbear.com>
2009-01-13 08:49:37 +0000

    Fixed saving of enable-override

--- src/java/com/eviware/soapui/actions/GlobalPropertiesPrefs.java
+++ src/java/com/eviware/soapui/actions/GlobalPropertiesPrefs.java
@@ -12,51 +12,66 @@
 
 package com.eviware.soapui.actions;
 
-import java.awt.Dimension;
-
 import com.eviware.soapui.impl.wsdl.panels.teststeps.support.PropertyHolderTable;
 import com.eviware.soapui.model.propertyexpansion.PropertyExpansionUtils;
 import com.eviware.soapui.model.settings.Settings;
+import com.eviware.soapui.settings.GlobalPropertySettings;
 import com.eviware.soapui.support.components.SimpleForm;
 import com.eviware.soapui.support.types.StringToStringMap;
+
+import java.awt.*;
 
 public class GlobalPropertiesPrefs implements Prefs
 {
    public final static String ENABLE_OVERRIDE = "Enable Override";
-	
-	public SimpleForm getForm()
-	{
-		SimpleForm form = new SimpleForm();
-		
-		PropertyHolderTable propertyHolderTable = new PropertyHolderTable( PropertyExpansionUtils.getGlobalProperties() );
-		propertyHolderTable.setPreferredSize( new Dimension( 200, 300 ) );
-		form.addComponent( propertyHolderTable);
-		form.addSpace();
-		form.appendCheckBox( ENABLE_OVERRIDE, "Enables overriding of any property-reference with global properties", false );
-		
-		return form;
-	}
+   private SimpleForm globalPropertiesForm;
 
-	public void getFormValues( Settings settings )
-	{
-	}
+   public SimpleForm getForm()
+   {
+      if( globalPropertiesForm == null )
+      {
+         globalPropertiesForm = new SimpleForm();
 
-	public String getTitle()
-	{
-		return "Global Properties";
-	}
+         PropertyHolderTable propertyHolderTable = new PropertyHolderTable(
+                 PropertyExpansionUtils.getGlobalProperties() );
+         propertyHolderTable.setPreferredSize( new Dimension( 200, 300 ) );
+         globalPropertiesForm.addComponent( propertyHolderTable );
+         globalPropertiesForm.addSpace();
+         globalPropertiesForm.appendCheckBox( ENABLE_OVERRIDE, "Enables overriding of any property-reference with global properties", false );
+      }
 
-	public StringToStringMap getValues( Settings settings )
-	{
-		return null;
-	}
+      return globalPropertiesForm;
 
-	public void setFormValues( Settings settings )
-	{
-      
-	}
+   }
 
-	public void storeValues( StringToStringMap values, Settings settings )
-	{
-	}
+   public void getFormValues( Settings settings )
+   {
+      StringToStringMap values = new StringToStringMap();
+      globalPropertiesForm.getValues( values );
+      storeValues( values, settings );
+   }
+
+   public String getTitle()
+   {
+      return "Global Properties";
+   }
+
+   public StringToStringMap getValues( Settings settings )
+   {
+      StringToStringMap values = new StringToStringMap();
+      values.put( ENABLE_OVERRIDE, settings.getBoolean( GlobalPropertySettings.ENABLE_OVERRIDE ) );
+      return values;
+   }
+
+   public void setFormValues( Settings settings )
+   {
+      globalPropertiesForm.setValues( getValues( settings ) );
+   }
+
+   public void storeValues( StringToStringMap values, Settings settings )
+   {
+      settings.setBoolean( GlobalPropertySettings.ENABLE_OVERRIDE,
+              values.getBoolean( ENABLE_OVERRIDE ) );
+   }
+
 }
