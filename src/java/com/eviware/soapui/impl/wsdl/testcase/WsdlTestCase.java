4a41033a5cf07f62e724b5b589f2c468f6970313
Ole Lensmar <ole.lensmar@smartbear.com>
2009-06-30 17:41:00 +0000

    added NP check

--- src/java/com/eviware/soapui/impl/wsdl/testcase/WsdlTestCase.java
+++ src/java/com/eviware/soapui/impl/wsdl/testcase/WsdlTestCase.java
@@ -266,7 +266,7 @@ public class WsdlTestCase extends AbstractTestPropertyHolderWsdlModelItem<TestCa
 		if( factory != null )
 		{
 			//Forward incompatibility, test for incompatible future version of HttpTestStep:
-			if(((RestRequestStepConfig)tsc.getConfig().changeType(RestRequestStepConfig.type)).getRestRequest() == null)
+			if( tsc.getConfig() != null && ((RestRequestStepConfig)tsc.getConfig().changeType(RestRequestStepConfig.type)).getRestRequest() == null)
 				return null;
 			
 			WsdlTestStep testStep = factory.buildTestStep( this, tsc, forLoadTest );
