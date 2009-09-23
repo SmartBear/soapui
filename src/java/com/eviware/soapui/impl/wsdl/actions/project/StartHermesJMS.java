7ad620cf00dc6434033146a3dbaa8d4ba3b2e1e3
Nebojsa Tasic <nebojsa.tasic@smartbear.com>
2009-09-23 14:28:39 +0000

    SOAPUI-546 Launch HermesJMS with specified config on project level , update

--- src/java/com/eviware/soapui/impl/wsdl/actions/project/StartHermesJMS.java
+++ src/java/com/eviware/soapui/impl/wsdl/actions/project/StartHermesJMS.java
@@ -11,6 +11,7 @@
  */
 package com.eviware.soapui.impl.wsdl.actions.project;
 
+import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
@@ -21,10 +22,8 @@ import com.eviware.soapui.model.propertyexpansion.PropertyExpander;
 import com.eviware.soapui.settings.ToolsSettings;
 import com.eviware.soapui.support.UISupport;
 import com.eviware.soapui.support.action.support.AbstractSoapUIAction;
-import com.eviware.x.form.XForm;
-import com.eviware.x.form.XFormDialog;
-import com.eviware.x.form.XFormDialogBuilder;
-import com.eviware.x.form.XFormFactory;
+import com.eviware.soapui.support.components.DirectoryFormComponent;
+import com.eviware.soapui.support.components.SimpleForm;
 
 public class StartHermesJMS extends AbstractSoapUIAction<WsdlProject>
 {
@@ -37,12 +36,13 @@ public class StartHermesJMS extends AbstractSoapUIAction<WsdlProject>
 
 	public void perform(WsdlProject project, Object param)
 	{
-
-		String hermesConfigPath = UISupport.prompt("Specify path to hermes-config.xml file", "Hermes configuration",
-				PropertyExpander.expandProperties(project, project.getHermesConfig()));
+		String hermesConfigPath = chooseFolderDialog(project);
+		
 		if (hermesConfigPath == null)
 			return;
 
+		project.setHermesConfig(hermesConfigPath);
+		
 		String hermesHome = SoapUI.getSettings().getString(ToolsSettings.HERMES_1_13, "");
 		if ("".equals(hermesHome))
 		{
@@ -64,5 +64,76 @@ public class StartHermesJMS extends AbstractSoapUIAction<WsdlProject>
 		}
 	}
 
+	private String chooseFolderDialog(WsdlProject project)
+	{
+		HermesConfigDialog chooseHermesConfigPath = new HermesConfigDialog(PropertyExpander.expandProperties(project, project.getHermesConfig()));
+				chooseHermesConfigPath.setVisible(true);
+		String hermesConfigPath = chooseHermesConfigPath.getPath();
+		return hermesConfigPath;
+	}
 
+	private SimpleForm toolsForm;
+
+	public SimpleForm getForm()
+	{
+		if (toolsForm == null)
+		{
+			toolsForm = new SimpleForm();
+			toolsForm.append("hermes-config.xml path", new DirectoryFormComponent(
+					"Location of desired HermesJMS configuration (hermes-config.xml)"));
+		}
+		return toolsForm;
+	}
+
+	private class HermesConfigDialog extends SimpleDialog
+	{
+
+		String path;
+		DirectoryFormComponent folderComponent;
+
+		public HermesConfigDialog(String initialPath)
+		{
+			super("Hermes configuration", "Location of desired HermesJMS configuration (hermes-config.xml)", null, true);
+			folderComponent.setValue(initialPath);
+			folderComponent.setInitialFolder(initialPath);
+
+		}
+
+		protected Component buildContent()
+		{
+
+			// name = new JTextField(20);
+			// editor = new ReportEditor();
+			// editor.setPreferredSize(new Dimension(500, 150));
+
+			SimpleForm form = new SimpleForm();
+			folderComponent = new DirectoryFormComponent(
+					"Location of desired HermesJMS configuration (hermes-config.xml)");
+//			folderComponent.setValue(getPath());
+//			String asdf=folderComponent.getValue();
+			form.addSpace(5);
+			form.append("Path", folderComponent);
+			form.addSpace(5);
+
+			return form.getPanel();
+		}
+
+		@Override
+		protected boolean handleOk()
+		{
+			setPath(folderComponent.getValue());
+			return true;
+		}
+
+		public String getPath()
+		{
+			return path;
+		}
+
+		public void setPath(String path)
+		{
+			this.path = path;
+		}
+
+	}
 }
