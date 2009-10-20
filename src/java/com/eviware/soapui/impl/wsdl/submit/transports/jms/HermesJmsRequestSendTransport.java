a72adbca48c08ca7139ffc56435ff633fce96815
Nebojsa Tasic <nebojsa.tasic@smartbear.com>
2009-10-20 13:12:46 +0000

    organize imports

--- src/java/com/eviware/soapui/impl/wsdl/submit/transports/jms/HermesJmsRequestSendTransport.java
+++ src/java/com/eviware/soapui/impl/wsdl/submit/transports/jms/HermesJmsRequestSendTransport.java
@@ -17,7 +17,6 @@ import hermes.Hermes;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
