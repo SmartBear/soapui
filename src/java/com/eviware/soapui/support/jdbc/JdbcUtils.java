c08c25df6bf9a5b42c56bcbf6056335eb68e8887
Ole Lensmar <ole.lensmar@smartbear.com>
2010-03-11 00:57:58 +0000

    added license

--- src/java/com/eviware/soapui/support/jdbc/JdbcUtils.java
+++ src/java/com/eviware/soapui/support/jdbc/JdbcUtils.java
@@ -1,3 +1,15 @@
+/*
+ *  soapUI, copyright (C) 2004-2010 eviware.com 
+ *
+ *  soapUI is free software; you can redistribute it and/or modify it under the 
+ *  terms of version 2.1 of the GNU Lesser General Public License as published by 
+ *  the Free Software Foundation.
+ *
+ *  soapUI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU Lesser General Public License for more details at gnu.org.
+ */
+
 package com.eviware.soapui.support.jdbc;
 
 import java.sql.Connection;
@@ -12,15 +24,16 @@ import com.eviware.soapui.model.propertyexpansion.PropertyExpansionContext;
 public class JdbcUtils
 {
 
-	public static final String PASS_TEMPLATE = "PASS_VALUE";	
+	public static final String PASS_TEMPLATE = "PASS_VALUE";
+
 	public static Connection testConnection( ModelItem modelItem, String driver, String connectionString, String password )
 			throws Exception, SQLException
 	{
 		PropertyExpansionContext context = new DefaultPropertyExpansionContext( modelItem );
-	
-		String drvr = PropertyExpander.expandProperties(context, driver).trim();
-		String connStr = PropertyExpander.expandProperties(context, connectionString).trim();
-		String pass = PropertyExpander.expandProperties(context, password).trim();
+
+		String drvr = PropertyExpander.expandProperties( context, driver ).trim();
+		String connStr = PropertyExpander.expandProperties( context, connectionString ).trim();
+		String pass = PropertyExpander.expandProperties( context, password ).trim();
 		if( connStr.contains( PASS_TEMPLATE ) )
 		{
 			connStr = connStr.replaceFirst( PASS_TEMPLATE, pass );
@@ -42,7 +55,7 @@ public class JdbcUtils
 			}
 		}
 		return DriverManager.getConnection( connStr );
-	
+
 	}
 
 }
