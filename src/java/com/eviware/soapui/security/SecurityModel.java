02620779168f4e82ef0e171c9154842785498974
Nebojsa Tasic <nebojsa.tasic@smartbear.com>
2010-11-04 15:33:40 +0000

    security model version 0.0.0.1

---
+++ src/java/com/eviware/soapui/security/SecurityModel.java
@@ -0,0 +1,35 @@
+/*
+ *  soapUI, copyright (C) 2004-2009 eviware.com 
+ *
+ *  soapUI is free software; you can redistribute it and/or modify it under the 
+ *  terms of version 2.1 of the GNU Lesser General Public License as published by 
+ *  the Free Software Foundation.
+ *
+ *  soapUI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU Lesser General Public License for more details at gnu.org.
+ */
+package com.eviware.soapui.security;
+
+import org.apache.commons.httpclient.HttpMethod;
+
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+
+public class SecurityModel
+{
+
+	
+//	private WsdlTestCase testCase;
+//	private HashMap<TestStep,List<SecurityAssertion>> hashMap;
+//	private String description;
+//	private String label;
+//	private SecurityModelConfig securityModelConfig;
+//	
+//	private SecurityResult securityResult;
+//	
+//	private SecurityAssertion[] securityAssertions;
+//	
+//	public void onemethod(){
+//		httpMethod.set
+//	}
+}
