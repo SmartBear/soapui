e50ae256e50c49e508ed9243c9c1897b7154673e
Nenad Ristic <nenad.ristic@smartbear.com>
2011-01-25 09:01:56 +0000

    Added the Large Attachment Security Check

---
+++ src/java/com/eviware/soapui/security/tools/InfiniteAttachment.java
@@ -0,0 +1,33 @@
+package com.eviware.soapui.security.tools;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.eviware.soapui.config.AttachmentConfig;
+import com.eviware.soapui.impl.support.AbstractHttpRequestInterface;
+import com.eviware.soapui.impl.wsdl.support.RequestFileAttachment;
+import com.eviware.soapui.model.iface.Attachment;
+
+public class InfiniteAttachment extends RequestFileAttachment{
+	private long maxSize;
+	
+	public InfiniteAttachment( AttachmentConfig config, AbstractHttpRequestInterface<?> request, long maxSize )
+	{
+		super( config, request);
+		this.maxSize = maxSize;
+	}
+	
+	public InputStream getInputStream() throws IOException
+	{
+		return new InfiniteInputStream(maxSize);
+	}
+	
+	public AttachmentType getAttachmentType() {
+		return Attachment.AttachmentType.UNKNOWN;
+	}
+	
+	@Override
+	public String getContentType() {
+		return "application/octet-stream";
+	}
+}
---
+++ src/java/com/eviware/soapui/security/tools/InfiniteInputStream.java
@@ -0,0 +1,25 @@
+package com.eviware.soapui.security.tools;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+public class InfiniteInputStream extends InputStream {
+	private long bytesSent = 0;
+	private long byteLimit;
+
+	public InfiniteInputStream(long maxSize) {
+		super();
+		byteLimit = maxSize;
+	}
+
+	@Override
+	public int read() throws IOException {
+		if (byteLimit > 0 && bytesSent > byteLimit) 
+			return -1;
+		Random rnd = new Random();
+		bytesSent++;
+		return rnd.nextInt();
+	}
+
+}
