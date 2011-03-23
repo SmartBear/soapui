2296cb45ea3966ad78b3e2c3f0d13dff213e4328
Robert nemet <robert.nemet@smartbear.com>
2011-03-23 16:21:48 +0000

    IN PROGRESS - issue SOAPUI-1293: as a Security tester I can edit the XPath statement for a Security Check Parameter https://jira.eviware.com/browse/SOAPUI-1293

--- src/java/com/eviware/soapui/security/ui/XPathCellRender.java
+++ src/java/com/eviware/soapui/security/ui/XPathCellRender.java
@@ -20,8 +20,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowFocusListener;
 
 import javax.swing.AbstractCellEditor;
 import javax.swing.JDialog;
