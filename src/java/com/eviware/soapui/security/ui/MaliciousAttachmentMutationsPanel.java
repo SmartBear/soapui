86d7ac830e07478f7647972bddd1ae94669a0674
Vladimir Cirkovic <vladimir.cirkovic@smartbear.com>
2011-04-21 13:43:19 +0000

    IN PROGRESS - issue SOAPUI-1438: implement design https://jira.eviware.com/browse/SOAPUI-1438

--- src/java/com/eviware/soapui/security/ui/MaliciousAttachmentMutationsPanel.java
+++ src/java/com/eviware/soapui/security/ui/MaliciousAttachmentMutationsPanel.java
@@ -11,32 +11,49 @@ import java.util.List;
 import java.util.Map;
 
 import javax.activation.MimetypesFileTypeMap;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellEditor;
+import javax.swing.text.Document;
 
 import org.jdesktop.swingx.JXTable;
 
 import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.config.MaliciousAttachmentSecurityCheckConfig;
 import com.eviware.soapui.model.security.MaliciousAttachmentsTableModel;
+import com.eviware.soapui.settings.ProjectSettings;
+import com.eviware.soapui.support.DocumentListenerAdapter;
 import com.eviware.soapui.support.UISupport;
 import com.eviware.soapui.support.components.JFormComponent;
+import com.eviware.soapui.support.components.JUndoableTextField;
+import com.eviware.soapui.support.components.JXToolBar;
 import com.eviware.soapui.support.types.StringList;
+import com.eviware.x.form.XForm.FieldType;
+import com.eviware.x.form.XFormTextField;
 import com.eviware.x.form.support.ADialogBuilder;
 import com.eviware.x.form.support.AField;
 import com.eviware.x.form.support.AField.AFieldType;
 import com.eviware.x.form.support.AForm;
+import com.eviware.x.impl.swing.AbstractSwingXFormField;
 import com.eviware.x.impl.swing.JFormDialog;
+import com.jgoodies.forms.builder.ButtonBarBuilder;
+import com.l2fprod.common.swing.JDirectoryChooser;
 
 public class MaliciousAttachmentMutationsPanel
 {
@@ -52,7 +69,7 @@ public class MaliciousAttachmentMutationsPanel
 		this.config = config;
 		dialog = ( JFormDialog )ADialogBuilder.buildDialog( MutationSettings.class );
 		dialog.getFormField( MutationSettings.MUTATIONS_PANEL ).setProperty( "component", createMutationsPanel() );
-		dialog.getFormField( MutationSettings.MUTATIONS_PANEL ).setProperty( "dimension", new Dimension( 620, 300 ) );
+		dialog.getFormField( MutationSettings.MUTATIONS_PANEL ).setProperty( "dimension", new Dimension( 620, 320 ) );
 		initDialog();
 	}
 
@@ -74,24 +91,57 @@ public class MaliciousAttachmentMutationsPanel
 	{
 		JPanel panel = new JPanel( new BorderLayout() );
 
-		JXTable generateTable = new JXTable( new GenerateMutationTableModel() );
-		JScrollPane generateTableScrollpane = new JScrollPane( generateTable );
-		generateTable.setPreferredScrollableViewportSize( new Dimension( 50, 50 ) );
-		generateTable.setFillsViewportHeight( true );
+		JScrollPane gP = buildTable( new GenerateMutationTableModel() );
+		JScrollPane rP = buildTable( new ReplaceMutationTableModel() );
 
-		JXTable replaceTable = new JXTable( new ReplaceMutationTableModel() );
-		JScrollPane replaceTableScrollPane = new JScrollPane( replaceTable );
-		replaceTable.setPreferredScrollableViewportSize( new Dimension( 50, 50 ) );
-		replaceTable.setFillsViewportHeight( true );
+		panel.add( gP, BorderLayout.NORTH );
+		panel.add( rP, BorderLayout.SOUTH );
 
-		panel.add( generateTableScrollpane, BorderLayout.NORTH );
-		panel.add( new JSeparator( SwingConstants.HORIZONTAL ) );
-		panel.add( replaceTableScrollPane, BorderLayout.SOUTH );
-		panel.add( new JSeparator( SwingConstants.HORIZONTAL ) );
-
-		panel.setBorder( BorderFactory.createEmptyBorder( 3, 3, 3, 3 ) );
+		panel.setBorder( BorderFactory.createEmptyBorder() );
 
 		return panel;
+	}
+
+	protected JScrollPane buildTable( MutationTableModel tableModel )
+	{
+		JPanel panel = new JPanel( new BorderLayout() );
+		JXTable table = new JXTable( tableModel );
+		setupTable( table );
+		JScrollPane tableScrollPane = new JScrollPane( table );
+
+		JXToolBar toolbar = UISupport.createToolbar();
+		toolbar.add( UISupport.createToolbarButton( new AddFileAction() ) );
+		toolbar.add( UISupport.createToolbarButton( new RemoveFileAction() ) );
+
+		panel.add( toolbar, BorderLayout.PAGE_START );
+		panel.add( tableScrollPane, BorderLayout.CENTER );
+		panel.setBorder( BorderFactory.createEmptyBorder() );
+
+		JScrollPane scrollPane = new JScrollPane( panel );
+		scrollPane.setBorder( BorderFactory.createEmptyBorder() );
+
+		return scrollPane;
+	}
+
+	protected void setupTable( JXTable table )
+	{
+		table.setPreferredScrollableViewportSize( new Dimension( 50, 90 ) );
+		table.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+		table.getTableHeader().setReorderingAllowed( false );
+		table.setDefaultEditor( String.class, getDefaultCellEditor() );
+		table.setSortable( false );
+
+		InternalCellRenderer internalCellRenderer = new InternalCellRenderer();
+
+		for( int c = 0; c < table.getColumnCount(); c++ )
+		{
+			table.getColumn( c ).setCellRenderer( internalCellRenderer );
+
+			if( c == table.getColumnCount() - 1 )
+			{
+				table.getColumn( c ).setPreferredWidth( 30 );
+			}
+		}
 	}
 
 	private Object createMutationsPanel()
@@ -99,10 +149,8 @@ public class MaliciousAttachmentMutationsPanel
 		JPanel panel = new JPanel( new BorderLayout() );
 
 		JSplitPane mainSplit = UISupport.createHorizontalSplit( buildFilesList(), buildTables() );
-		mainSplit.setResizeWeight( 0.20 );
+		mainSplit.setResizeWeight( 0.2 );
 		panel.add( mainSplit, BorderLayout.CENTER );
-
-		panel.setBorder( BorderFactory.createEmptyBorder( 3, 3, 3, 3 ) );
 
 		return panel;
 	}
@@ -378,6 +426,225 @@ public class MaliciousAttachmentMutationsPanel
 			}
 
 			return null;
+		}
+	}
+
+	public class AddFileAction extends AbstractAction
+	{
+		public AddFileAction()
+		{
+			putValue( Action.SMALL_ICON, UISupport.createImageIcon( "/add_property.gif" ) );
+			putValue( Action.SHORT_DESCRIPTION, "Add file" );
+		}
+
+		public void actionPerformed( ActionEvent e )
+		{
+			// String newToken = "";
+			// newToken = UISupport.prompt( "Enter token", "New Token", newToken );
+			// String newValue = "";
+			// newValue = UISupport.prompt( "Enter description", "New Description",
+			// newValue );
+			//
+			// sensitivInformationTableModel.addToken( newToken, newValue );
+
+		}
+	}
+
+	public class RemoveFileAction extends AbstractAction
+	{
+		public RemoveFileAction()
+		{
+			putValue( Action.SMALL_ICON, UISupport.createImageIcon( "/remove_property.gif" ) );
+			putValue( Action.SHORT_DESCRIPTION, "Remove file" );
+		}
+
+		public void actionPerformed( ActionEvent e )
+		{
+
+			// sensitivInformationTableModel.removeRows(
+			// tokenTable.getSelectedRows() );
+
+		}
+	}
+
+	class FileFormField extends AbstractSwingXFormField<JPanel> implements XFormTextField
+	{
+		private JTextField textField;
+		private final FieldType type;
+		private JButton selectDirectoryButton;
+		private String projectRoot;
+
+		private boolean updating;
+		private String oldValue;
+		private String currentDirectory;
+
+		public FileFormField( String tooltip, FieldType type )
+		{
+			super( new JPanel() );
+			this.type = type;
+
+			ButtonBarBuilder builder = new ButtonBarBuilder( getComponent() );
+			textField = new JUndoableTextField( 30 );
+			textField.setToolTipText( tooltip );
+			builder.addGriddedGrowing( textField );
+			builder.addRelatedGap();
+			selectDirectoryButton = new JButton( new SelectDirectoryAction() );
+			builder.addFixed( selectDirectoryButton );
+
+			textField.getDocument().addDocumentListener( new DocumentListenerAdapter()
+			{
+
+				@Override
+				public void update( Document document )
+				{
+					String text = textField.getText();
+
+					if( !updating )
+						fireValueChanged( text, oldValue );
+
+					oldValue = text;
+				}
+			} );
+		}
+
+		public void setValue( String value )
+		{
+			updating = true;
+			oldValue = null;
+			updateValue( value );
+			updating = false;
+		}
+
+		private void updateValue( String value )
+		{
+			if( value != null && projectRoot != null && value.startsWith( projectRoot ) )
+			{
+				if( value.equals( projectRoot ) )
+					value = "";
+				else if( value.length() > projectRoot.length() + 1 )
+					value = value.substring( projectRoot.length() + 1 );
+			}
+
+			textField.setText( value );
+		}
+
+		public String getValue()
+		{
+			String text = textField.getText().trim();
+
+			if( projectRoot != null && text.length() > 0 )
+			{
+				String tempName = projectRoot + File.separatorChar + text;
+				if( new File( tempName ).exists() )
+				{
+					text = tempName;
+				}
+			}
+
+			return text;
+		}
+
+		public void setEnabled( boolean enabled )
+		{
+			textField.setEnabled( enabled );
+			selectDirectoryButton.setEnabled( enabled );
+		}
+
+		@Override
+		public boolean isEnabled()
+		{
+			return textField.isEnabled();
+		}
+
+		public void setCurrentDirectory( String currentDirectory )
+		{
+			this.currentDirectory = currentDirectory;
+		}
+
+		public class SelectDirectoryAction extends AbstractAction
+		{
+			private JFileChooser fileChooser;
+
+			public SelectDirectoryAction()
+			{
+				super( "Browse..." );
+			}
+
+			public void actionPerformed( ActionEvent e )
+			{
+				if( fileChooser == null )
+				{
+					if( type == FieldType.FILE_OR_FOLDER )
+					{
+						fileChooser = new JFileChooser();
+						fileChooser.setFileSelectionMode( JFileChooser.FILES_AND_DIRECTORIES );
+					}
+					else if( type == FieldType.FOLDER || type == FieldType.PROJECT_FOLDER )
+						fileChooser = new JDirectoryChooser();
+					else
+						fileChooser = new JFileChooser();
+
+				}
+
+				String value = FileFormField.this.getValue();
+				if( value.length() > 0 )
+				{
+					fileChooser.setSelectedFile( new File( value ) );
+				}
+				else if( currentDirectory != null )
+				{
+					fileChooser.setCurrentDirectory( new File( currentDirectory ) );
+				}
+				else if( projectRoot != null )
+				{
+					fileChooser.setCurrentDirectory( new File( projectRoot ) );
+				}
+
+				int returnVal = fileChooser.showOpenDialog( UISupport.getMainFrame() );
+				if( returnVal == JFileChooser.APPROVE_OPTION )
+				{
+					updateValue( fileChooser.getSelectedFile().getAbsolutePath() );
+				}
+			}
+		}
+
+		public void setProperty( String name, Object value )
+		{
+			super.setProperty( name, value );
+
+			if( name.equals( ProjectSettings.PROJECT_ROOT ) && type == FieldType.PROJECT_FOLDER )
+			{
+				projectRoot = ( String )value;
+			}
+			else if( name.equals( CURRENT_DIRECTORY ) )
+			{
+				currentDirectory = ( String )value;
+			}
+		}
+
+		public void setWidth( int columns )
+		{
+			textField.setColumns( columns );
+		}
+
+		public String getCurrentDirectory()
+		{
+			return currentDirectory;
+		}
+	}
+
+	protected TableCellEditor getDefaultCellEditor()
+	{
+		return new XPathCellRender();
+	}
+
+	private class InternalCellRenderer extends DefaultTableCellRenderer
+	{
+		public InternalCellRenderer()
+		{
+			super();
+
+			setHorizontalAlignment( SwingConstants.CENTER );
 		}
 	}
 
