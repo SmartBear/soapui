b1352e83ff110c715b152a78ead297c12fca7985
Henrik Olsson <henrik.olsson@smartbear.com>
2011-06-10 12:20:47 +0000

    "Request delay(ms)" => "Request delay (ms)"

--- src/java/com/eviware/soapui/security/ui/SecurityConfigurationDialogBuilder.java
+++ src/java/com/eviware/soapui/security/ui/SecurityConfigurationDialogBuilder.java
@@ -31,13 +31,14 @@ public class SecurityConfigurationDialogBuilder
 		@AField( description = "Strategy", name = "Select strategy", type = AFieldType.RADIOGROUP )
 		public final static String STRATEGY = "Select strategy";
 
-		@AField( description = "Request Delay", name = "Request Delay(ms)", type = AFieldType.INT )
-		public final static String DELAY = "Request Delay(ms)";
+		@AField( description = "Request Delay", name = "Request Delay (ms)", type = AFieldType.INT )
+		public final static String DELAY = "Request Delay (ms)";
 
 		@AField( description = "Apply to Failed TestSteps", name = "Apply to Failed TestSteps", type = AFieldType.BOOLEAN )
 		public final static String APPLY_TO_FAILED_STEPS = "Apply to Failed TestSteps";
 
-		//indicates if security scan should run only once in case of DataSource Loop involved
+		// indicates if security scan should run only once in case of DataSource
+		// Loop involved
 		@AField( description = "Run only once", name = "Run only once", type = AFieldType.BOOLEAN )
 		public final static String RUN_ONLY_ONCE = "Run only once";
 
