9181dd7a562de245fa1e93774dfc3506ed6794f7
Dragica Soldo <dragica.soldo@smartbear.com>
2011-07-21 14:44:22 +0000

    IN PROGRESS - issue SOAPUI-2432: Update installations ( install4j ) to use downloader https://jira.eviware.com/browse/SOAPUI-2432

---
+++ src/install4j/loadUI/loadUIInstall.bat
@@ -0,0 +1,1 @@
+java -jar downloader-1.0-SNAPSHOT.jar http://www.eviware.com/nightly-builds/loadui/1.0.2-SNAPSHOT/loadUI-1_0_2-SNAPSHOT.exe
---
+++ src/install4j/loadUI/loadUIInstallMAC.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar downloader-1.0-SNAPSHOT.jar http://www.eviware.com/nightly-builds/loadui/1.0.2-SNAPSHOT/loadUI_macos_1_5_1-SNAPSHOT.dmg
