f5cae137add1dbabed76a967d374529ba9bd8515
Vladimir Cirkovic <vladimir.cirkovic@smartbear.com>
2011-12-27 09:57:07 +0000

    IN PROGRESS - issue SOAPUI-3214: Implement proxy functionality https://jira.eviware.com/browse/SOAPUI-3214

--- src/install4j/loadUI/loadUIInstall.bat
+++ src/install4j/loadUI/loadUIInstall.bat
@@ -1,2 +1,2 @@
 rem loadUI.temp.path will be replaced in the installer with userdir/tempLoadUI
-"soapui.home\jre\bin\java" -jar "soapui.home\lib\downloader-1.0-SNAPSHOT.jar" "loadUI.download.url" "loadUI.temp.path"
+"soapui.home\jre\bin\java" -jar "soapui.home\lib\downloader-1.0-SNAPSHOT.jar" "loadUI.download.url" "loadUI.temp.path" %*
