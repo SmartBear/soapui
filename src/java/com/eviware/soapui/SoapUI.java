4584bcb9eb8a509f15ef5078249d1e2890feef7c
Robert nemet <robert.nemet@smartbear.com>
2012-02-20 12:44:18 +0000

    OPEN - issue SOAPUI-3457: Update license to 4.5 Beta 1 https://jira.eviware.com/browse/SOAPUI-3457

--- src/java/com/eviware/soapui/SoapUI.java
+++ src/java/com/eviware/soapui/SoapUI.java
@@ -171,7 +171,7 @@ public class SoapUI
 	public static final String DEFAULT_DESKTOP = "Default";
 	public static final String CURRENT_SOAPUI_WORKSPACE = SoapUI.class.getName() + "@workspace";
 	public final static Logger log = Logger.getLogger( SoapUI.class );
-	public final static String SOAPUI_VERSION = "4.5.0-SNAPSHOT";
+	public final static String SOAPUI_VERSION = "4.5.0-Beta 1";
 	public static final String DEFAULT_WORKSPACE_FILE = "default-soapui-workspace.xml";
 	public static final String SOAPUI_SPLASH = "soapui-splash.png";
 	public static final String SOAPUI_TITLE = "/com/eviware/soapui/resources/branded/branded.properties";
