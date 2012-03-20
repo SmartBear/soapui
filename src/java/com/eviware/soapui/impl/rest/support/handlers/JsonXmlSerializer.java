c7b487cacd2076b3ae330011a7e3a6bdee47de34
Ole Lensmar <ole.lensmar@smartbear.com>
2012-03-20 12:50:37 +0000

    added modification comment

--- src/java/com/eviware/soapui/impl/rest/support/handlers/JsonXmlSerializer.java
+++ src/java/com/eviware/soapui/impl/rest/support/handlers/JsonXmlSerializer.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Modifications by Ole Lensmar
+ * Copyright 2012 SmartBear Software
  */
 
 package com.eviware.soapui.impl.rest.support.handlers;
