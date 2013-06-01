d2497219acd63f547afda05e2e42090bfafaa4bd
Jon Williams <djangofan@gmail.com>
2013-06-01 11:58:06 -0700

    Updated testrunner.bat script.
    Additional fuctionality in the testrunner.bat script.

--- soapui-installer/src/dist/bin/testrunner.bat
+++ soapui-installer/src/dist/bin/testrunner.bat
@@ -1,31 +1,103 @@
-@echo off
+@ECHO off
+SETLOCAL ENABLEDELAYEDEXPANSION
+TITLE Script %~nx0 running from %~dp0
 
-set SOAPUI_HOME=%~dp0
-set JAVA=%JAVA_HOME%\bin\java
+:::::::::::::::::::::::::::::::::
+:: testrunner.bat
+:: for SoapUI
+:: pre-configuration and execution script for SoapUITestCaseRunner
+::
+:::::::::::::::::::::::::::::::::
 
-if not "%JAVA_HOME%" == "" goto SET_CLASSPATH
+:::::::::::::::::::::::::::::::::
+:: Jar file name set by Maven build system
+:: Modify as desired
+:::::::::::::::::::::::::::::::::
+SET JARFILE=${project.src.artifactId}-${project.version}.jar
 
-set JAVA=java
+ECHO Args passed to %~nx0 :
+FOR %%I IN (%*) DO ECHO %%I
+ECHO.
 
-echo JAVA_HOME is not set, unexpected results may occur.
-echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+:: check if configured jar file exists
+IF NOT EXIST "%SOAPUI_HOME%\bin\%JARFILE%" (
+  ECHO The JARFILE variable configured in this script is not pointing to an existing jar file:
+  ECHO     ^"%SOAPUI_HOME%\bin\%JARFILE%^"
+  GOTO :ERROR
+)
 
-:SET_CLASSPATH
+:: Set SOAPUI_HOME and make sure script runs from bin directory.
+CALL :verifyparentfolder bin
 
-rem init classpath
+IF EXIST "%SOAPUI_HOME%\jre\bin" (
+  ECHO Using embedded version of Java at ^"%SOAPUI_HOME%\jre\bin\java.exe^".
+  SET "JAVA=%SOAPUI_HOME%\jre\bin\java.exe"
+) ELSE (
+  IF NOT DEFINED JAVA_HOME (
+    ECHO JAVA_HOME is not set, unexpected results may occur with %~nx0 .
+    ECHO Set JAVA_HOME to the directory of your local JDK to avoid this message.
+    SET JAVA=java.exe
+  ) ELSE (
+    ECHO Using Java defined by JAVA_HOME.
+    SET "JAVA=%JAVA_HOME%\bin\java.exe"
+  )
+)
+ECHO.
 
-set CLASSPATH=%SOAPUI_HOME%${project.src.artifactId}-${project.version}.jar;%SOAPUI_HOME%..\lib\*;
+:::::::::::::::::::::::::::::::::
+:: Ability to prepend CLASSPATH with libraries if you define PRE_CLASSPATH before
+:: calling this script. Otherwise place libraries in the 'soapui.ext.libraries' directory.
+:::::::::::::::::::::::::::::::::
+IF NOT DEFINED CLASSPATH SET CLASSPATH=.
+IF DEFINED PRE_CLASSPATH SET "CLASSPATH=%PRE_CLASSPATH%;%CLASSPATH%"
+SET "CLASSPATH=%CLASSPATH%;%SOAPUI_HOME%\bin\%JARFILE%;%SOAPUI_HOME%\lib\*"
 
-rem JVM parameters, modify as appropriate
-set JAVA_OPTS=-Xms128m -Xmx1024m -Dsoapui.properties=soapui.properties "-Dsoapui.home=%SOAPUI_HOME%\"
+:::::::::::::::::::::::::::::::::
+:: JVM parameters. Modify as desired.
+:::::::::::::::::::::::::::::::::
+SET JAVA_OPTS=-Xms128m -Xmx1024m -D^"soapui.properties=%BIN_HOME%\soapui.properties^" -D^"soapui.home=%BIN_HOME%^"
+SET JAVA_OPTS=%JAVA_OPTS% -D^"soapui.ext.libraries=%SOAPUI_HOME%\bin\ext^"
+SET JAVA_OPTS=%JAVA_OPTS% -D^"soapui.ext.listeners=%SOAPUI_HOME%\bin\listeners^"
+SET JAVA_OPTS=%JAVA_OPTS% -D^"soapui.ext.actions=%SOAPUI_HOME%\bin\actions^"
 
-if "%SOAPUI_HOME%\" == "" goto START
-    set JAVA_OPTS=%JAVA_OPTS% -Dsoapui.ext.libraries="%SOAPUI_HOME%ext"
-    set JAVA_OPTS=%JAVA_OPTS% -Dsoapui.ext.listeners="%SOAPUI_HOME%listeners"
-    set JAVA_OPTS=%JAVA_OPTS% -Dsoapui.ext.actions="%SOAPUI_HOME%actions"
+:::::::::::::::::::::::::::::::::
+:: Start SoapUI.
+:::::::::::::::::::::::::::::::::
+ECHO ----------------------------------------
+ECHO -- Running soapui testcase runner...
+ECHO -- Implicit classpath: %CLASSPATH%
+ECHO -- Java opts: %JAVA_OPTS%
+ECHO ----------------------------------------
+ECHO.
+"%JAVA%" %JAVA_OPTS% com.eviware.soapui.tools.SoapUITestCaseRunner %*
 
-:START
+GOTO :END
 
-rem ********* run soapui testcase runner ***********
+:::::::::::::::::::::::::::::::::
+:: Function to verify parent folder name of script 
+:::::::::::::::::::::::::::::::::
+:verifyparentfolder dirName
+SET "BIN_HOME=%~dp0"
+IF "%BIN_HOME:~-1%"=="\" SET "BIN_HOME=%BIN_HOME:~0,-1%"
+ECHO Current directory is: %BIN_HOME%
+FOR /F "delims=" %%I IN ("%BIN_HOME%") DO (
+  SET THISFOLDER=%%~nI
+)
+ECHO This folder name is ^"%THISFOLDER%^".
+IF "%~1"=="%THISFOLDER%" (
+  SET SOAPUI_HOME=!BIN_HOME:\%THISFOLDER%=!
+  ECHO SOAPUI_HOME: %SOAPUI_HOME%
+) ELSE (
+  ECHO Function arg ^"%~1^" must match actual parent folder name.
+  ECHO This script may not be running from the expected folder.
+  GOTO :ERROR
+)
+EXIT /B 0
 
-"%JAVA%" %JAVA_OPTS% -cp "%CLASSPATH%" com.eviware.soapui.tools.SoapUITestCaseRunner %*
+:ERROR
+ECHO There was an error in the %~nx0 script.
+PING.exe -n 10 -w 1000 127.0.0.1>NUL
+
+:END
+ECHO The script %~nx0 is finished.
+ECHO.
