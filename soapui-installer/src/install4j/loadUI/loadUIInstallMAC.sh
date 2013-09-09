3ad7cccf767e746428cac2a28de96c52702f1c0a
anders.jaensson <anders.jaensson@smartbear.com>
2013-09-09 09:21:07 +0200

    SOAP-397: Auto detect latest loadui version in installer

--- soapui-installer/src/install4j/loadUI/loadUIInstallMAC.sh
+++ soapui-installer/src/install4j/loadUI/loadUIInstallMAC.sh
@@ -1,2 +1,1 @@
-#loadUI.temp.path will be replaced in the installer with userdir/tempLoadUI
-java -jar "soapui.home/lib/downloader-1.0-SNAPSHOT.jar" "loadUI.download.url" "loadUI.temp.path" "loadUI.proxy.host" "loadUI.proxy.port"
+java -jar "soapui.home/lib/downloader-1.0-SNAPSHOT.jar" "loadUI.download.url" "loadUI.proxy.host" "loadUI.proxy.port"
