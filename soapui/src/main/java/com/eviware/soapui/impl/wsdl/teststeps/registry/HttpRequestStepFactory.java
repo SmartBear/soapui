bccf3a38fdd4b92c11ae4dcd35bdd468358499fb
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2013-09-11 17:00:02 +0200

    Show editable buttons to http test step

--- soapui/src/main/java/com/eviware/soapui/impl/wsdl/teststeps/registry/HttpRequestStepFactory.java
+++ soapui/src/main/java/com/eviware/soapui/impl/wsdl/teststeps/registry/HttpRequestStepFactory.java
@@ -82,7 +82,7 @@ public class HttpRequestStepFactory extends WsdlTestStepFactory
 
 		params = new XmlBeansRestParamsTestPropertyHolder( testCase, RestParametersConfig.Factory.newInstance() );
 
-		paramsTable = new RestParamsTable( params, false, NewRestResourceActionBase.ParamLocation.RESOURCE, false, false );
+		paramsTable = new RestParamsTable( params, false, NewRestResourceActionBase.ParamLocation.RESOURCE, true, false );
 		dialog.getFormField( Form.PARAMSTABLE ).setProperty( "component", paramsTable );
 		dialog.setValue( Form.STEPNAME, name );
 
