ba02a5770de72e409e94853502424a2d9ef5434a
anders.jaensson <anders.jaensson@smartbear.com>
2013-09-20 11:00:47 +0200

    Temp fix for NPE

--- soapui/src/test/java/com/eviware/soapui/utils/StubbedDialogs.java
+++ soapui/src/test/java/com/eviware/soapui/utils/StubbedDialogs.java
@@ -1,9 +1,10 @@
 package com.eviware.soapui.utils;
 
+import com.eviware.soapui.support.NullProgressDialog;
 import com.eviware.x.dialogs.XDialogs;
 import com.eviware.x.dialogs.XProgressDialog;
 
-import java.awt.Dimension;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,103 +14,103 @@ import java.util.List;
 public class StubbedDialogs implements XDialogs
 {
 
-	private List<String> errorMessages = new ArrayList<String>(  );
-	private List<String> infoMessages = new ArrayList<String>(  );
+	private List<String> errorMessages = new ArrayList<String>();
+	private List<String> infoMessages = new ArrayList<String>();
 
 	@Override
 	public void showErrorMessage( String message )
 	{
-		errorMessages.add(message);
+		errorMessages.add( message );
 	}
 
 	@Override
 	public void showInfoMessage( String message )
 	{
-		infoMessages.add(message);
+		infoMessages.add( message );
 	}
 
 	@Override
 	public void showInfoMessage( String message, String title )
 	{
-		infoMessages.add(message);
+		infoMessages.add( message );
 	}
 
 	@Override
 	public void showExtendedInfo( String title, String description, String content, Dimension size )
 	{
-		
+
 	}
 
 	@Override
 	public boolean confirm( String question, String title )
 	{
-		return false;  
+		return false;
 	}
 
 	@Override
 	public Boolean confirmOrCancel( String question, String title )
 	{
-		return null;  
+		return null;
 	}
 
 	@Override
 	public int yesYesToAllOrNo( String question, String title )
 	{
-		return 0;  
+		return 0;
 	}
 
 	@Override
 	public String prompt( String question, String title, String value )
 	{
-		return null;  
+		return null;
 	}
 
 	@Override
 	public String prompt( String question, String title )
 	{
-		return null;  
+		return null;
 	}
 
 	@Override
 	public Object prompt( String question, String title, Object[] objects )
 	{
-		return null;  
+		return null;
 	}
 
 	@Override
 	public Object prompt( String question, String title, Object[] objects, String value )
 	{
-		return null;  
+		return null;
 	}
 
 	@Override
 	public char[] promptPassword( String question, String title )
 	{
-		return new char[0];  
+		return new char[0];
 	}
 
 	@Override
 	public XProgressDialog createProgressDialog( String label, int length, String initialValue, boolean canCancel )
 	{
-		return null;  
+		return new NullProgressDialog();
 	}
 
 	@Override
 	public boolean confirmExtendedInfo( String title, String description, String content, Dimension size )
 	{
-		return false;  
+		return false;
 	}
 
 	@Override
 	public Boolean confirmOrCancleExtendedInfo( String title, String description, String content, Dimension size )
 	{
-		return null;  
+		return null;
 	}
 
 	@Override
 	public String selectXPath( String title, String info, String xml, String xpath )
 	{
-		return null;  
+		return null;
 	}
 
 	public List<String> getErrorMessages()
