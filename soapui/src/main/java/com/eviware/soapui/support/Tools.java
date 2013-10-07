9e0c2b4e71cd0ae843f015bbef850a675e9fd168
anders.jaensson <anders.jaensson@smartbear.com>
2013-10-07 10:44:42 +0200

    Use default browser on all platforms

--- soapui/src/main/java/com/eviware/soapui/support/Tools.java
+++ soapui/src/main/java/com/eviware/soapui/support/Tools.java
@@ -12,6 +12,7 @@
 
 package com.eviware.soapui.support;
 
+import java.awt.*;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -25,6 +26,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.ArrayList;
@@ -174,47 +176,9 @@ public class Tools
 
 	public static void openURL( String url )
 	{
-		String osName = System.getProperty( "os.name" );
-
 		try
 		{
-			if( osName.startsWith( "Mac OS" ) )
-			{
-				Class<?> fileMgr = Class.forName( "com.apple.eio.FileManager" );
-				Method openURL = fileMgr.getDeclaredMethod( "openURL", new Class[] { String.class } );
-				openURL.invoke( null, new Object[] { url } );
-			}
-			else if( osName.startsWith( "Windows" ) )
-			{
-				if( url.startsWith( "file:" ) )
-				{
-					url = URLDecoder.decode( url.substring( 5 ), "utf-8" );
-					while( url.startsWith( "/" ) )
-						url = url.substring( 1 );
-
-					url = url.replace( '/', '\\' );
-
-					if( !new File( url ).exists() )
-					{
-						UISupport.showErrorMessage( "File [" + url + "] not found" );
-						return;
-					}
-				}
-
-				Runtime.getRuntime().exec( "rundll32 url.dll,FileProtocolHandler " + url );
-			}
-			else
-			{ // assume Unix or Linux
-				String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
-				String browser = null;
-				for( int count = 0; count < browsers.length && browser == null; count++ )
-					if( Runtime.getRuntime().exec( new String[] { "which", browsers[count] } ).waitFor() == 0 )
-						browser = browsers[count];
-				if( browser == null )
-					throw new Exception( "Could not find web browser" );
-				else
-					Runtime.getRuntime().exec( new String[] { browser, url } );
-			}
+			Desktop.getDesktop().browse( new URI( url ) );
 		}
 		catch( Exception e )
 		{
