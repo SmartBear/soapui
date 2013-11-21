f28a8a492a14de18ad13801b104222ea968b3960
spindelmanne <manne.fagerlind@smartbear.com>
2013-11-21 13:44:37 +0100

    Added error logging when JMS session is not found

---
+++ soapui/src/main/java/com/eviware/soapui/impl/rest/actions/support/ParamLocation.java
@@ -0,0 +1,13 @@
+package com.eviware.soapui.impl.rest.actions.support;
+
+/**
+* Created with IntelliJ IDEA.
+* User: manne
+* Date: 11/18/13
+* Time: 1:34 PM
+* To change this template use File | Settings | File Templates.
+*/
+public enum ParamLocation
+{
+	RESOURCE, METHOD
+}
