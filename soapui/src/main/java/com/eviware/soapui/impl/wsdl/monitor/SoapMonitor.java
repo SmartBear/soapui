2766f62c878ee1da3da303b9d8b375ebad892675
spindelmanne <manne.fagerlind@smartbear.com>
2014-01-13 13:06:33 +0100

    Classes BrowserComponent and WebRecordingSettings removed

--- soapui/src/main/java/com/eviware/soapui/impl/wsdl/monitor/SoapMonitor.java
+++ soapui/src/main/java/com/eviware/soapui/impl/wsdl/monitor/SoapMonitor.java
@@ -12,41 +12,6 @@
 
 package com.eviware.soapui.impl.wsdl.monitor;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JScrollPane;
-import javax.swing.JToggleButton;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.table.AbstractTableModel;
-
-import org.apache.commons.collections.list.TreeList;
-import org.jdesktop.swingx.JXTable;
-import org.jdesktop.swingx.decorator.Filter;
-import org.jdesktop.swingx.decorator.FilterPipeline;
-import org.jdesktop.swingx.decorator.PatternFilter;
-
 import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.impl.support.AbstractInterface;
 import com.eviware.soapui.impl.wsdl.WsdlInterface;
@@ -78,7 +43,6 @@ import com.eviware.soapui.settings.ProxySettings;
 import com.eviware.soapui.support.DateUtil;
 import com.eviware.soapui.support.StringUtils;
 import com.eviware.soapui.support.UISupport;
-import com.eviware.soapui.support.components.BrowserComponent;
 import com.eviware.soapui.support.components.JComponentInspector;
 import com.eviware.soapui.support.components.JInspectorPanel;
 import com.eviware.soapui.support.components.JInspectorPanelFactory;
@@ -93,6 +57,38 @@ import com.eviware.x.form.support.AField;
 import com.eviware.x.form.support.AField.AFieldType;
 import com.eviware.x.form.support.AForm;
 import com.jgoodies.forms.builder.ButtonBarBuilder;
+import org.apache.commons.collections.list.TreeList;
+import org.jdesktop.swingx.JXTable;
+import org.jdesktop.swingx.decorator.Filter;
+import org.jdesktop.swingx.decorator.FilterPipeline;
+import org.jdesktop.swingx.decorator.PatternFilter;
+
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * A SOAP Monitor..
@@ -776,7 +772,7 @@ public class SoapMonitor extends JPanel
 						HttpTestRequestStep test = ( HttpTestRequestStep )testCase.insertTestStep(
 								httpRequestStepFactory.createConfig( me, "Monitor Request " + ( row + 1 ) ), -1 );
 
-						test.getTestRequest().setRequestHeaders( excludeHeaders( me.getRequestHeaders() ) );
+						test.getTestRequest().setRequestHeaders( excludeProxyHeaders( me.getRequestHeaders() ) );
 
 						HttpTestRequest request = ( HttpTestRequest )test.getHttpRequest();
 
@@ -1058,18 +1054,17 @@ public class SoapMonitor extends JPanel
 	}
 
 	/**
-	 * excludes proxy headers as well as headers set for excluding in Global
-	 * Preferences/WebRecordingSettings.EXCLUDED_HEADERS
+	 * Excludes proxy headers
 	 * 
 	 * @param requestHeaders
 	 * @return
 	 */
-	private StringToStringsMap excludeHeaders( StringToStringsMap requestHeaders )
+	private StringToStringsMap excludeProxyHeaders( StringToStringsMap requestHeaders )
 	{
 		StringToStringsMap stsmap = new StringToStringsMap();
 		for( String key : requestHeaders.getKeys() )
 		{
-			if( !( key.contains( "Proxy" ) || key.contains( "Content" ) ) && !BrowserComponent.isHeaderExcluded( key ) )
+			if( !( key.contains( "Proxy" ) || key.contains( "Content" ) ) )
 			{
 				stsmap.add( key, requestHeaders.get( key, "" ) );
 			}
