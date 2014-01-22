1f621f3d190a52b75badc63c3294bc826d97ac7c
Marc Paquette <marcpa@mac.com>
2014-01-21 22:33:55 -0500

    Remove unnecessary `extends`.

--- soapui/src/main/java/com/eviware/soapui/model/iface/Script.java
+++ soapui/src/main/java/com/eviware/soapui/model/iface/Script.java
@@ -1,16 +1,9 @@
 package com.eviware.soapui.model.iface;
 
-import com.eviware.soapui.model.ModelItem;
-
 /**
- * Created with IntelliJ IDEA.
- * User: marcpa
- * Date: 2013-12-11
- * Time: 06:50
- * To change this template use File | Settings | File Templates.
+ * String constants used when dealing with script test steps.
  */
-public interface Script extends ModelItem {
+public interface Script {
     final static String RESULT_PROPERTY = "result";
     final static String SCRIPT_PROPERTY = "script";
-
 }
