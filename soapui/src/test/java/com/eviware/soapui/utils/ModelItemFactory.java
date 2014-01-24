b6511a847d1f913357285d6517360fb7c316c621
Prakash Jat <prakash.jat@smartbear.com>
2014-01-24 13:16:11 +0100

    SOAP-1251: Property expansion in reuqest content now works for HTTP and REST requests also

--- soapui/src/test/java/com/eviware/soapui/utils/ModelItemFactory.java
+++ soapui/src/test/java/com/eviware/soapui/utils/ModelItemFactory.java
@@ -1,13 +1,7 @@
 package com.eviware.soapui.utils;
 
-import com.eviware.soapui.config.OperationConfig;
-import com.eviware.soapui.config.RestMethodConfig;
-import com.eviware.soapui.config.RestRequestConfig;
-import com.eviware.soapui.config.RestResourceConfig;
-import com.eviware.soapui.config.RestServiceConfig;
-import com.eviware.soapui.config.TestCaseConfig;
-import com.eviware.soapui.config.TestSuiteConfig;
-import com.eviware.soapui.config.WsdlInterfaceConfig;
+
+import com.eviware.soapui.config.*;
 import com.eviware.soapui.impl.WorkspaceImpl;
 import com.eviware.soapui.impl.rest.RestMethod;
 import com.eviware.soapui.impl.rest.RestRequest;
@@ -18,6 +12,7 @@ import com.eviware.soapui.impl.wsdl.WsdlOperation;
 import com.eviware.soapui.impl.wsdl.WsdlProject;
 import com.eviware.soapui.impl.wsdl.WsdlTestSuite;
 import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+import com.eviware.soapui.impl.wsdl.teststeps.WsdlTestRequestStep;
 import com.eviware.soapui.model.workspace.WorkspaceFactory;
 import com.eviware.soapui.support.SoapUIException;
 import com.eviware.soapui.support.types.StringToStringMap;
@@ -29,42 +24,47 @@ public class ModelItemFactory
 {
 	public static RestRequest makeRestRequest() throws SoapUIException
 	{
-			return new RestRequest( makeRestMethod(), RestRequestConfig.Factory.newInstance(), false);
+		return new RestRequest( makeRestMethod(), RestRequestConfig.Factory.newInstance(), false );
 	}
 
-	public static RestRequest makeRestRequest(RestResource restResource) throws SoapUIException
+	public static RestRequest makeRestRequest( RestResource restResource ) throws SoapUIException
 	{
-		return new RestRequest( makeRestMethod(restResource), RestRequestConfig.Factory.newInstance(), false);
+		return new RestRequest( makeRestMethod( restResource ), RestRequestConfig.Factory.newInstance(), false );
 	}
 
 	private static RestMethod makeRestMethod( RestResource restResource )
 	{
-		return new RestMethod( restResource, RestMethodConfig.Factory.newInstance());
+		return new RestMethod( restResource, RestMethodConfig.Factory.newInstance() );
 	}
 
 	public static RestMethod makeRestMethod() throws SoapUIException
 	{
-		return new RestMethod( makeRestResource(), RestMethodConfig.Factory.newInstance());
+		return new RestMethod( makeRestResource(), RestMethodConfig.Factory.newInstance() );
 	}
 
 	public static RestResource makeRestResource() throws SoapUIException
 	{
-		return new RestResource( makeRestService(), RestResourceConfig.Factory.newInstance());
+		return new RestResource( makeRestService(), RestResourceConfig.Factory.newInstance() );
 	}
 
 	public static RestService makeRestService() throws SoapUIException
 	{
-		return new RestService(makeWsdlProject(), RestServiceConfig.Factory.newInstance());
+		return new RestService( makeWsdlProject(), RestServiceConfig.Factory.newInstance() );
 	}
 
 	public static WsdlProject makeWsdlProject() throws SoapUIException
 	{
-		return new WsdlProject( (WorkspaceImpl )WorkspaceFactory.getInstance().openWorkspace( "testWorkSpace" , new StringToStringMap()));
+		return new WsdlProject( ( WorkspaceImpl )WorkspaceFactory.getInstance().openWorkspace( "testWorkSpace", new StringToStringMap() ) );
 	}
 
 	public static WsdlTestCase makeTestCase() throws SoapUIException
 	{
 		return new WsdlTestCase( new WsdlTestSuite( makeWsdlProject(), TestSuiteConfig.Factory.newInstance() ), TestCaseConfig.Factory.newInstance(), false );
+	}
+
+	public static WsdlTestRequestStep makeTestRequestStep() throws SoapUIException
+	{
+		return new WsdlTestRequestStep( makeTestCase(), TestStepConfig.Factory.newInstance(), false );
 	}
 
 	public static WsdlOperation makeWsdlOperation() throws SoapUIException
