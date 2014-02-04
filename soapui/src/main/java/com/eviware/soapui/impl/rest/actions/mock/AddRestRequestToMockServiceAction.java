69f211d573750214fa698590217bc9bda29b046d
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2014-02-04 17:27:44 +0100

    Supports adding request to existing mock service

--- soapui/src/main/java/com/eviware/soapui/impl/rest/actions/mock/AddRestRequestToMockServiceAction.java
+++ soapui/src/main/java/com/eviware/soapui/impl/rest/actions/mock/AddRestRequestToMockServiceAction.java
@@ -6,7 +6,9 @@ import com.eviware.soapui.impl.rest.mock.RestMockAction;
 import com.eviware.soapui.impl.rest.mock.RestMockResponse;
 import com.eviware.soapui.impl.rest.mock.RestMockService;
 import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.impl.wsdl.mock.WsdlMockService;
 import com.eviware.soapui.impl.wsdl.submit.transports.http.HttpResponse;
+import com.eviware.soapui.model.support.ModelSupport;
 import com.eviware.soapui.support.MessageSupport;
 import com.eviware.soapui.support.UISupport;
 import com.eviware.soapui.support.action.support.AbstractSoapUIAction;
@@ -14,12 +16,12 @@ import com.eviware.soapui.support.action.support.AbstractSoapUIAction;
 public class AddRestRequestToMockServiceAction extends AbstractSoapUIAction<RestRequest>
 {
 
+	private static final String CREATE_MOCKSUITE_OPTION = "Create new..";
 	public static final String SOAPUI_ACTION_ID = "AddRestRequestToMockServiceAction";
-
 	private static final MessageSupport messages = MessageSupport.getMessages( AddRestRequestToMockServiceAction.class );
 
 
-	public AddRestRequestToMockServiceAction( )
+	public AddRestRequestToMockServiceAction()
 	{
 		super( messages.get( "Title" ), messages.get( "Description" ) );
 	}
@@ -27,16 +29,39 @@ public class AddRestRequestToMockServiceAction extends AbstractSoapUIAction<Rest
 	@Override
 	public void perform( RestRequest restRequest, Object param )
 	{
-      String title = getName();
-      WsdlProject project = restRequest.getRestMethod().getInterface().getProject();
+		String title = getName();
 
-		String defaultName = "MockService " + (project.getMockServiceCount() + 1);
-      String mockServiceName = UISupport.prompt("Enter name of new MockService", title, defaultName);
+		RestMockService mockService = null;
+		WsdlProject project = restRequest.getOperation().getInterface().getProject();
 
-      RestMockService mockService = project.addNewRestMockService( mockServiceName );
+		while( mockService == null )
+		{
+
+			if( project.getRestMockServiceCount() > 0 )
+			{
+				String[] mockServices = ModelSupport.getNames( project.getRestMockServiceList(),
+						new String[] { CREATE_MOCKSUITE_OPTION } );
+
+				// prompt
+				String option = UISupport.prompt( "Select RESTMockService for adding REST request", title, mockServices );
+				if( option == null )
+					return;
+
+				mockService = project.getRestMockServiceByName( option );
+			}
+
+			if( mockService == null )
+			{
+
+				String defaultName = "MockService " + ( project.getRestMockServiceCount() + 1 );
+				String mockServiceName = UISupport.prompt( "Enter name of new MockService", title, defaultName );
+
+				mockService = project.addNewRestMockService( mockServiceName );
+			}
+		}
 
 		addNewMockAction( restRequest, mockService );
-   }
+	}
 
 	private void addNewMockAction( RestRequest restRequest, RestMockService mockService )
 	{
