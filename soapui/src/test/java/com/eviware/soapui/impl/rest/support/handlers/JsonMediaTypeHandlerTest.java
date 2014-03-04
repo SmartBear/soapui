0770b54a32ab0a1d035dc0c5dafa657b2f077dff
spindelmanne <manne.fagerlind@smartbear.com>
2014-03-04 11:31:52 +0100

    SOAP-1408 Cleaned up unit test (still two ugly stack traces though)

--- soapui/src/test/java/com/eviware/soapui/impl/rest/support/handlers/JsonMediaTypeHandlerTest.java
+++ soapui/src/test/java/com/eviware/soapui/impl/rest/support/handlers/JsonMediaTypeHandlerTest.java
@@ -6,28 +6,28 @@ import com.eviware.soapui.impl.wsdl.submit.transports.http.ExtendedHttpMethod;
 import com.eviware.soapui.impl.wsdl.submit.transports.http.HttpResponse;
 import com.eviware.soapui.impl.wsdl.submit.transports.http.SinglePartHttpResponse;
 import com.eviware.soapui.impl.wsdl.submit.transports.http.support.metrics.SoapUIMetrics;
+import com.eviware.soapui.model.iface.Request;
 import com.eviware.soapui.model.iface.SubmitContext;
 import com.eviware.soapui.model.propertyexpansion.PropertyExpansionContext;
 import org.apache.commons.httpclient.URI;
+import org.apache.commons.httpclient.URIException;
 import org.apache.http.Header;
 import org.apache.http.ProtocolVersion;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.io.HttpTransportMetrics;
 import org.junit.Test;
 
 import java.net.URISyntaxException;
 
 import static com.eviware.soapui.utils.ModelItemFactory.makeRestRequest;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * Created with IntelliJ IDEA.
- * User: manne
- * Date: 3/3/14
- * Time: 10:49 AM
- * To change this template use File | Settings | File Templates.
+ * Unit tests for the JsonMediaTypeHandler class.
  */
 public class JsonMediaTypeHandlerTest
 {
@@ -35,54 +35,56 @@ public class JsonMediaTypeHandlerTest
 	public static final String ENDPOINT = "http://somehost.com";
 
 	@Test
-	public void retainsOriginalUriAsNamespaceUri() throws Exception
+	public void retainsUriInFirstSubmitAsNamespaceUri() throws Exception
 	{
 		RestRequest restRequest = makeRestRequest();
 		restRequest.setEndpoint( ENDPOINT );
 		String originalPath = "/original/path";
 		restRequest.setPath( originalPath );
 
-		SubmitContext mock = mock( SubmitContext.class );
-		ExtendedHttpMethod httpMethod = prepareHttpMethodWith( originalPath );
-		when(mock.getProperty( BaseHttpRequestTransport.HTTP_METHOD )).thenReturn( httpMethod );
-		when(mock.getProperty( BaseHttpRequestTransport.REQUEST_URI )).thenReturn( new URI( ENDPOINT + originalPath ) );
-		restRequest.submit( mock, false);
+		submitRequest( restRequest, originalPath );
 		HttpResponse response = makeResponseFor( restRequest, originalPath );
 
 		JsonMediaTypeHandler handler = new JsonMediaTypeHandler();
 		String originalXml = handler.createXmlRepresentation( response );
-		System.out.println( "originalXml = " + originalXml );
 		String anotherPath = "/another/path";
 		restRequest.setPath( anotherPath );
-		httpMethod = prepareHttpMethodWith( originalPath );
-		when(mock.getProperty( BaseHttpRequestTransport.HTTP_METHOD )).thenReturn( httpMethod );
-		when(mock.getProperty( BaseHttpRequestTransport.REQUEST_URI )).thenReturn(new URI( ENDPOINT + anotherPath ));
-		restRequest.submit(mock(SubmitContext.class), false);
+		submitRequest( restRequest, anotherPath );
 		HttpResponse responseWithNewPath = makeResponseFor( restRequest, anotherPath );
-		assertThat(handler.createXmlRepresentation( responseWithNewPath), is(originalXml) );
+		assertThat( handler.createXmlRepresentation( responseWithNewPath ), is( equalTo( originalXml ) ) );
+	}
+
+	private SubmitContext submitRequest( RestRequest restRequest, String originalPath ) throws URISyntaxException, URIException, Request.SubmitException
+	{
+		SubmitContext mock = mock( SubmitContext.class );
+		HttpRequestBase httpMethod = mock( HttpRequestBase.class );
+		when( mock.getProperty( BaseHttpRequestTransport.HTTP_METHOD ) ).thenReturn( httpMethod );
+		when( mock.getProperty( BaseHttpRequestTransport.REQUEST_URI ) ).thenReturn( new URI( ENDPOINT + originalPath ) );
+		restRequest.submit( mock, false );
+		return mock;
 	}
 
 	private SinglePartHttpResponse makeResponseFor( RestRequest restRequest, String originalPath ) throws URISyntaxException
 	{
-		ExtendedHttpMethod httpMethod = prepareHttpMethodWith(originalPath);
+		ExtendedHttpMethod httpMethod = prepareHttpMethodWith( originalPath );
 		SinglePartHttpResponse response =
 				new SinglePartHttpResponse( restRequest, httpMethod, null, mock( PropertyExpansionContext.class ) );
-		response.setResponseContent( "{ firstName: 'Kalle', secondName: 'Ek' }"  );
+		response.setResponseContent( "{ firstName: 'Kalle', secondName: 'Ek' }" );
 		return response;
 	}
 
 	private ExtendedHttpMethod prepareHttpMethodWith( String path ) throws URISyntaxException
 	{
-		ExtendedHttpMethod httpMethod = mock(ExtendedHttpMethod.class);
-		when(httpMethod.getResponseContentType()).thenReturn( "text/json" );
-		when(httpMethod.getMethod()).thenReturn( "GET" );
-		when(httpMethod.getProtocolVersion()).thenReturn( new ProtocolVersion( "http", 1, 1 ) );
+		ExtendedHttpMethod httpMethod = mock( ExtendedHttpMethod.class );
+		when( httpMethod.getResponseContentType() ).thenReturn( "text/json" );
+		when( httpMethod.getMethod() ).thenReturn( "GET" );
+		when( httpMethod.getProtocolVersion() ).thenReturn( new ProtocolVersion( "http", 1, 1 ) );
 		SoapUIMetrics soapUIMetrics = new SoapUIMetrics( mock( HttpTransportMetrics.class ),
 				mock( HttpTransportMetrics.class ) );
-		when(httpMethod.getMetrics()).thenReturn( soapUIMetrics );
-		when(httpMethod.getAllHeaders()).thenReturn( new Header[0] );
+		when( httpMethod.getMetrics() ).thenReturn( soapUIMetrics );
+		when( httpMethod.getAllHeaders() ).thenReturn( new Header[0] );
 		when( httpMethod.getResponseReadTime() ).thenReturn( 10L );
-		when(httpMethod.getURI()).thenReturn( new java.net.URI( ENDPOINT + path) );
+		when( httpMethod.getURI() ).thenReturn( new java.net.URI( ENDPOINT + path ) );
 		return httpMethod;
 	}
 }
