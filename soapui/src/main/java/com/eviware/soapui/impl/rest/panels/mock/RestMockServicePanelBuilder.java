d62caf85f45b0b3eaf036321d8024c79b2ce1408
Fredrik Rubensson <fredrik@eldfluga.se>
2014-03-06 13:07:03 +0100

    path and port not editable from options panel

--- soapui/src/main/java/com/eviware/soapui/impl/rest/panels/mock/RestMockServicePanelBuilder.java
+++ soapui/src/main/java/com/eviware/soapui/impl/rest/panels/mock/RestMockServicePanelBuilder.java
@@ -31,8 +31,8 @@ public class RestMockServicePanelBuilder extends EmptyPanelBuilder<RestMockServi
 		boolean editable = true;
 		table.addProperty( "Name", "name", editable );
 		table.addProperty( "Description", "description", editable );
-		table.addProperty( "Path", "path", editable );
-		table.addProperty( "Port", "port", editable );
+		table.addProperty( "Path", "path", !editable );
+		table.addProperty( "Port", "port", !editable );
 		table.addProperty( "Dispatch Responses", "dispatchResponseMessages", JPropertiesTable.BOOLEAN_OPTIONS );
 		table.setPropertyObject( mockService );
 
