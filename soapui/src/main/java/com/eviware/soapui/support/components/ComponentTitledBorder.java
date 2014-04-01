67059c7c632fb8aff2255e955eb6ea69d181aff3
spindelmanne <manne.fagerlind@smartbear.com>
2014-04-01 13:59:42 +0200

    SOAP-1876 Final licence updates in SoapUI OS

--- soapui/src/main/java/com/eviware/soapui/support/components/ComponentTitledBorder.java
+++ soapui/src/main/java/com/eviware/soapui/support/components/ComponentTitledBorder.java
@@ -31,20 +31,6 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 
-/**
- * MySwing: Advanced Swing Utilites Copyright (C) 2005 Santhosh Kumar T
- * <p/>
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- * <p/>
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 public class ComponentTitledBorder implements Border, MouseListener, SwingConstants
 {
 	int offset = 5;
--- soapui/src/main/java/com/eviware/soapui/support/editor/inspectors/wsrm/WsdlRequestWsrmInspector.java
+++ soapui/src/main/java/com/eviware/soapui/support/editor/inspectors/wsrm/WsdlRequestWsrmInspector.java
@@ -34,18 +34,6 @@ import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
 
-/*
- *  SoapUI, copyright (C) 2004-2012 smartbear.com
- *
- *  SoapUI is free software; you can redistribute it and/or modify it under the
- *  terms of version 2.1 of the GNU Lesser General Public License as published by 
- *  the Free Software Foundation.
- *
- *  SoapUI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- *  See the GNU Lesser General Public License for more details at gnu.org.
- */
-
 public class WsdlRequestWsrmInspector extends AbstractWsrmInspector implements XmlInspector, SubmitListener
 {
 	private final WsdlRequest request;
