f730eab728e02c9120a6fcadd45e208cf555011c
spindelmanne <manne.fagerlind@smartbear.com>
2014-05-15 09:19:43 +0200

    Introduced PluginManager

---
+++ soapui/src/main/java/com/eviware/soapui/plugins/AvailablePlugin.java
@@ -0,0 +1,33 @@
+/*
+ *  SoapUI, copyright (C) 2004-2014 smartbear.com
+ *
+ *  SoapUI is free software; you can redistribute it and/or modify it under the
+ *  terms of version 2.1 of the GNU Lesser General Public License as published by 
+ *  the Free Software Foundation.
+ *
+ *  SoapUI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU Lesser General Public License for more details at gnu.org.
+ */
+package com.eviware.soapui.plugins;
+
+import java.net.URL;
+
+public class AvailablePlugin {
+
+    private final PluginInfo pluginInfo;
+    private final URL url;
+
+    public AvailablePlugin(PluginInfo pluginInfo, URL url) {
+        this.pluginInfo = pluginInfo;
+        this.url = url;
+    }
+
+    public PluginInfo getPluginInfo() {
+        return pluginInfo;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+}
