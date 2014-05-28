7abaa40495a1e524aa233718e5cf87fccc1dfaaa
Ole Lensmar <ole.lensmar@smartbear.com>
2014-05-27 22:42:39 -0400

    Pulling api-monitoring-rca-integration-SOAP2382 to next

---
+++ soapui/src/main/java/com/eviware/soapui/model/propertyexpansion/resolvers/providers/CurrentStepIndexProvider.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2004-2014 SmartBear Software
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the Licence for the specific language governing permissions and limitations
+ * under the Licence.
+*/
+
+package com.eviware.soapui.model.propertyexpansion.resolvers.providers;
+
+import com.eviware.soapui.model.propertyexpansion.PropertyExpansionContext;
+import com.eviware.soapui.model.propertyexpansion.resolvers.DynamicPropertyResolver;
+import com.eviware.soapui.model.testsuite.TestCaseRunContext;
+
+/**
+ * Returns the current teststep index
+ */
+
+public class CurrentStepIndexProvider implements DynamicPropertyResolver.ValueProvider {
+    @Override
+    public String getValue(PropertyExpansionContext context) {
+        if( context instanceof TestCaseRunContext )
+        {
+            return String.valueOf( ((TestCaseRunContext)context).getCurrentStepIndex());
+        }
+
+        return null;
+    }
+}
