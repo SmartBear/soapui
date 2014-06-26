955d65e290426e65596a7a7ade57b626c4a1e6a2
Prakash Jat <prakash.jat@smartbear.com>
2014-06-26 15:09:15 +0200

    Create RestRequestTespStep through ModelItemFactory for unit tests: SOAP-2096

--- soapui/src/test/java/com/eviware/soapui/utils/ModelItemFactory.java
+++ soapui/src/test/java/com/eviware/soapui/utils/ModelItemFactory.java
@@ -15,7 +15,22 @@
 */
 package com.eviware.soapui.utils;
 
-import com.eviware.soapui.config.*;
+import com.eviware.soapui.config.MockOperationConfig;
+import com.eviware.soapui.config.MockResponseConfig;
+import com.eviware.soapui.config.MockServiceConfig;
+import com.eviware.soapui.config.OAuth2ProfileConfig;
+import com.eviware.soapui.config.OAuth2ProfileContainerConfig;
+import com.eviware.soapui.config.OperationConfig;
+import com.eviware.soapui.config.RESTMockResponseConfig;
+import com.eviware.soapui.config.RESTMockServiceConfig;
+import com.eviware.soapui.config.RestMethodConfig;
+import com.eviware.soapui.config.RestRequestConfig;
+import com.eviware.soapui.config.RestResourceConfig;
+import com.eviware.soapui.config.RestServiceConfig;
+import com.eviware.soapui.config.TestCaseConfig;
+import com.eviware.soapui.config.TestStepConfig;
+import com.eviware.soapui.config.TestSuiteConfig;
+import com.eviware.soapui.config.WsdlInterfaceConfig;
 import com.eviware.soapui.impl.WorkspaceImpl;
 import com.eviware.soapui.impl.rest.DefaultOAuth2ProfileContainer;
 import com.eviware.soapui.impl.rest.OAuth2Profile;
@@ -36,6 +51,7 @@ import com.eviware.soapui.impl.wsdl.mock.WsdlMockOperation;
 import com.eviware.soapui.impl.wsdl.mock.WsdlMockResponse;
 import com.eviware.soapui.impl.wsdl.mock.WsdlMockService;
 import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCase;
+import com.eviware.soapui.impl.wsdl.teststeps.RestTestRequestStep;
 import com.eviware.soapui.impl.wsdl.teststeps.WsdlTestRequestStep;
 import com.eviware.soapui.model.workspace.WorkspaceFactory;
 import com.eviware.soapui.support.SoapUIException;
@@ -89,6 +105,13 @@ public class ModelItemFactory {
 
     public static WsdlTestRequestStep makeTestRequestStep() throws SoapUIException {
         return new WsdlTestRequestStep(makeTestCase(), TestStepConfig.Factory.newInstance(), false);
+    }
+
+    public static RestTestRequestStep makeRestTestRequestStep() throws Exception {
+        RestTestRequestStep restTestRequestStep = new RestTestRequestStep(makeTestCase(),
+                TestStepConfig.Factory.newInstance(), false);
+        restTestRequestStep.getConfig().setConfig(ModelItemFactory.makeRestRequest().getConfig());
+        return restTestRequestStep;
     }
 
     public static WsdlOperation makeWsdlOperation() throws SoapUIException {
