72eb4d850153cb97cf4c1e177818aaae6538ddb6
Alexander.Yeremeyev <Alexander.Yeremeyev@smartbear.com>
2015-01-27 15:25:54 +0300

    SOAP-3308-improve-analytics-info part-01

--- soapui/src/main/java/com/eviware/soapui/SoapUI.java
+++ soapui/src/main/java/com/eviware/soapui/SoapUI.java
@@ -23,10 +23,6 @@ import com.eviware.soapui.actions.StartHermesJMSButtonAction;
 import com.eviware.soapui.actions.SwitchDesktopPanelAction;
 import com.eviware.soapui.actions.VersionUpdateAction;
 import com.eviware.soapui.analytics.Analytics;
-import com.eviware.soapui.analytics.AnalyticsManager;
-import com.eviware.soapui.analytics.providers.GoogleAnalyticsProviderFactory;
-import com.eviware.soapui.analytics.providers.KeenIOProviderFactory;
-import com.eviware.soapui.analytics.providers.LogTabAnalyticsProvider;
 import com.eviware.soapui.impl.WorkspaceImpl;
 import com.eviware.soapui.impl.actions.ImportWsdlProjectAction;
 import com.eviware.soapui.impl.actions.NewWsdlProjectAction;
@@ -134,7 +130,6 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JTabbedPane;
@@ -653,8 +648,8 @@ public class SoapUI {
 
     private static final class SoapUIRunner implements Runnable {
         public void run() {
-            initializeAnalytics();
-
+            AnalyticHelper.InitializeAnalytic();
+            Analytics.trackSessionStart();
             boolean isDebug = java.lang.management.ManagementFactory.getRuntimeMXBean().
                     getInputArguments().toString().indexOf("-agentlib:jdwp") > 0;
             if (isDebug) {
@@ -704,42 +699,6 @@ public class SoapUI {
             } catch (Exception e) {
                 e.printStackTrace();
                 System.exit(1);
-            }
-        }
-
-        private static boolean analyticsDisabled() {
-            Settings settings = SoapUI.getSettings();
-            boolean disableAnalytics = settings.getBoolean(UISettings.DISABLE_ANALYTICS, true);
-            if (!disableAnalytics) {
-                return false;
-            }
-            Version optOutVersion = new Version(settings.getString(UISettings.ANALYTICS_OPT_OUT_VERSION, "0.0"));
-            Version currentSoapUIVersion = new Version(SoapUI.SOAPUI_VERSION);
-            if (!optOutVersion.getMajorVersion().equals(currentSoapUIVersion.getMajorVersion())) {
-                disableAnalytics = JOptionPane.showConfirmDialog(null, "Do you want to help us improve SoapUI by sending anonymous usage statistics?",
-                        "Usage statistics", JOptionPane.YES_NO_OPTION) == JOptionPane.NO_OPTION;
-                settings.setBoolean(UISettings.DISABLE_ANALYTICS, disableAnalytics);
-            }
-            if (disableAnalytics) {
-                settings.setString(UISettings.ANALYTICS_OPT_OUT_VERSION, currentSoapUIVersion.getMajorVersion());
-            }
-            return disableAnalytics;
-        }
-
-
-        private void initializeAnalytics() {
-
-            AnalyticsManager manager = Analytics.getAnalyticsManager();
-            manager.setExecutorService(SoapUI.getThreadPool());
-            //We send non-anonymous (license) data to Keen IO anyway, even if user has opted out
-            manager.registerAnalyticsProviderFactory(new KeenIOProviderFactory());
-            if (analyticsDisabled()) {
-                return;
-            }
-
-            manager.registerAnalyticsProviderFactory(new GoogleAnalyticsProviderFactory());
-            if (System.getProperty("soapui.analytics.logtab", "false").equals("true")) {
-                manager.registerAnalyticsProviderFactory(new LogTabAnalyticsProvider.LogTabAnalyticsProviderFactory());
             }
         }
 
