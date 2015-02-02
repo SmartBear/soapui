a639dda8a331145a8fa1b86107bb813ca20f370c
IlyaAvdeev <ilya.avdeev@smartbear.com>
2015-02-02 10:29:03 +0300

    Sending OS user contact information + refactoring.

--- soapui/src/main/java/com/eviware/soapui/analytics/providers/BaseAnalyticsProvider.java
+++ soapui/src/main/java/com/eviware/soapui/analytics/providers/BaseAnalyticsProvider.java
@@ -2,11 +2,18 @@ package com.eviware.soapui.analytics.providers;
 
 import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.analytics.AnalyticsProvider;
+import com.eviware.soapui.settings.ProxySettings;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.DataOutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketAddress;
+import java.net.URL;
 
 /**
  * Created by aleshin on 5/15/2014.
@@ -67,6 +74,77 @@ public abstract class BaseAnalyticsProvider implements AnalyticsProvider {
         e.printStackTrace(new PrintWriter(output));
         String stackTraceWithoutLineBreaks = output.toString().replaceAll("(\r|\n)+", " / ");
         return stackTraceWithoutLineBreaks.replaceAll("\\s+/\\s+", " / ");
+    }
+
+    protected HttpURLConnection initializeConnection(String connectionURL) {
+
+        HttpURLConnection connection;
+
+        try {
+            URL url = new URL(connectionURL);
+            String host = SoapUI.getSettings().getString(ProxySettings.HOST, "");
+            int port = 0;
+
+            try {
+                port = Integer.parseInt(SoapUI.getSettings().getString(ProxySettings.PORT, "0"));
+            } catch (NumberFormatException ex) {
+            }
+
+            if (SoapUI.getSettings().getBoolean(ProxySettings.ENABLE_PROXY, false) && host.compareTo("") != 0 && port != 0) {
+                SocketAddress sa = new InetSocketAddress(host, port);
+                Proxy proxy = new Proxy(Proxy.Type.HTTP, sa);
+                connection = (HttpURLConnection) url.openConnection(proxy);
+            } else {
+                connection = (HttpURLConnection) url.openConnection();
+            }
+
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            return connection;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    protected boolean sendRecord(String connectionURL, String parameters) {
+
+        if (parameters == null) {
+            return false;
+        }
+
+        HttpURLConnection connection = initializeConnection(connectionURL);
+        if (connection == null) {
+            return false;
+        }
+
+        try {
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(parameters);
+            wr.flush();
+            wr.close();
+
+            int responseCode = connection.getResponseCode();
+            return responseCode == 200;
+            /* This code usefull for debugging. Do not delete
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return true;
+            //*/
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
