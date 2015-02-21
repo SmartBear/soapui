8fedf8e1dfa41b4f77fb39bc3bafa7958849ea6c
docudoc <andreas.jonson@smartbear.com>
2015-02-21 20:27:01 +0100

    HelpUrlChecker update

--- soapui/src/test/java/com/eviware/soapui/support/HelpUrlChecker.java
+++ soapui/src/test/java/com/eviware/soapui/support/HelpUrlChecker.java
@@ -5,7 +5,8 @@ import com.eviware.soapui.impl.wsdl.support.HelpUrls;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
+
 
 /**
  * @author joel.jonsson
@@ -18,17 +19,25 @@ public class HelpUrlChecker {
     private static void openHelpUrls(Class<?> helpUrlsClass) throws IllegalAccessException, InterruptedException, IOException {
         for (Field field : helpUrlsClass.getDeclaredFields()) {
             String url = (String) field.get(null);
-            if (!StringUtils.isNullOrEmpty(url) ) {
-                try {
-                    int responseCode = ((HttpURLConnection) new URL(url).openConnection()).getResponseCode();
-                    if (responseCode != 200) {
+            if (!StringUtils.isNullOrEmpty(url)) {
+                String helpUrl = url;
+                if (url.substring(0, 1).equals("/")) {
+                    helpUrl = HelpUrls.BASE_URL_PROD + url;
+                }
 
-                        System.out.println(responseCode + " " + url);
+                try {
+                    int responseCode = ((HttpURLConnection) new URL(helpUrl).openConnection()).getResponseCode();
+                    if (responseCode != 200) {
+                        System.out.println(responseCode + " " + helpUrl);
                     }
+                } catch (UnknownHostException e) {
+                    System.out.println("No host: " + helpUrl);
                 } catch (Exception e) {
-                    System.out.println("Error: " + url);
+                    System.out.println("Error: " + helpUrl);
                     e.printStackTrace();
                 }
+
+
             }
 //            Thread.sleep(2000);
         }
