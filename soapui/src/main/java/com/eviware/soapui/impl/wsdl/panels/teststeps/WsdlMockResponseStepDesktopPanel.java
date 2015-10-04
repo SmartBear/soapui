5b2a2dedfa18ebe8721859f4b28e7031562e4c5c
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2015-10-04 19:30:04 +0200

    The teststep called SOAP Mock Response was missing four buttons in its toolbar. This is caused by a missing call to the overwritten parent method.
    This fix re-enables the missing buttons.

--- soapui/src/main/java/com/eviware/soapui/impl/wsdl/panels/teststeps/WsdlMockResponseStepDesktopPanel.java
+++ soapui/src/main/java/com/eviware/soapui/impl/wsdl/panels/teststeps/WsdlMockResponseStepDesktopPanel.java
@@ -139,7 +139,10 @@ public class WsdlMockResponseStepDesktopPanel extends AbstractWsdlMockResponseDe
 
     @Override
     protected void createToolbar(JXToolBar toolbar) {
-        toolbar.addUnrelatedGap();
+        
+        super.createToolbar(toolbar);
+    	
+    	toolbar.addUnrelatedGap();
         toolbar.addFixed(new JLabel("Path"));
         toolbar.addRelatedGap();
         pathField = new JTextField(getModelItem().getPath(), 15);
