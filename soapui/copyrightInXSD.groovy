afefb850b0c975332714f4ecc6cd33a91f61a2da
Ole Lensmar <ole.lensmar@smartbear.com>
2016-11-11 11:32:03 +0100

    updated xsd license

--- soapui/copyrightInXSD.groovy
+++ soapui/copyrightInXSD.groovy
@@ -10,7 +10,7 @@ System.exit(0)
 def currentYear = new Date().getYear()+1900
 def copyright = """<?xml version="1.0" encoding="UTF-8"?>
 <!--
-  ~ SoapUI, copyright (C) 2004-${currentYear} SmartBear Software
+  ~ Copyright 2004-${currentYear} SmartBear Software
   ~
   ~ Licensed under the EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent
   ~ versions of the EUPL (the "Licence");
@@ -23,7 +23,8 @@ def copyright = """<?xml version="1.0" encoding="UTF-8"?>
   ~ distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
   ~ express or implied. See the Licence for the specific language governing permissions and limitations
   ~ under the Licence.
- -->
+-->
+
 """
 
 def updatedfiles=[]
