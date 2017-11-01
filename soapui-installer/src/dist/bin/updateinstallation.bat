ef85cb4a9fb4c90da2938e593685a37e4ac8149e
Alexander Chernyaev <alexander.chernyaev@smartbear.com>
2017-11-01 08:39:48 +0300

    SOAPUI-259 Fix parameters

--- soapui-installer/src/dist/bin/updateinstallation.bat
+++ soapui-installer/src/dist/bin/updateinstallation.bat
@@ -1,2 +1,2 @@
 set CLASSPATH=..\lib\*
-..\jre\bin\java -cp "%CLASSPATH%" TrackOutAppAction UpdateSoftware Product SoapUIOS Version $1 success true
+..\jre\bin\java -cp "%CLASSPATH%" TrackOutAppAction UpdateSoftware Product SoapUIOS Version %1 success true
