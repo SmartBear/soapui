5e407dcdc5aeb72c95f5c3f7db503cd7726ecf83
Alexander Chernyaev <alexander.chernyaev@smartbear.com>
2017-11-16 10:00:34 +0300

    SOAPUIOS-329 Update year in copyrights

--- soapui/src/main/java/com/eviware/soapui/support/editor/inspectors/script/ScriptInspectorFactory.java
+++ soapui/src/main/java/com/eviware/soapui/support/editor/inspectors/script/ScriptInspectorFactory.java
@@ -1,5 +1,5 @@
 /*
- * SoapUI, Copyright (C) 2004-2016 SmartBear Software 
+ * SoapUI, Copyright (C) 2004-2017 SmartBear Software
  *
  * Licensed under the EUPL, Version 1.1 or - as soon as they will be approved by the European Commission - subsequent 
  * versions of the EUPL (the "Licence"); 
