SmartBear/soapui

commit 8f921d6983235e268513bed9837bc5d56d90941c

Read payload for REST mock request

soapui/src/main/java/com/eviware/soapui/impl/wsdl/mock/WsdlMockRequest.java
@@ -16,42 +16,23 @@
 
 package com.eviware.soapui.impl.wsdl.mock;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Enumeration;
-import java.util.Vector;
-
-import javax.mail.MessagingException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.impl.support.AbstractMockRequest;
 import com.eviware.soapui.impl.wsdl.WsdlProject;
-import com.eviware.soapui.model.mock.MockService;
-import com.eviware.soapui.model.settings.Settings;
+import com.eviware.soapui.impl.wsdl.support.soap.SoapUtils;
+import com.eviware.soapui.impl.wsdl.support.soap.SoapVersion;
+import com.eviware.soapui.impl.wsdl.support.wss.IncomingWss;
+import com.eviware.soapui.support.StringUtils;
+import com.eviware.soapui.support.xml.XmlUtils;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.w3c.dom.Document;
 
-import com.eviware.soapui.SoapUI;
-import com.eviware.soapui.impl.wsdl.WsdlOperation;
-import com.eviware.soapui.impl.wsdl.submit.transports.http.support.attachments.MockRequestDataSource;
-import com.eviware.soapui.impl.wsdl.submit.transports.http.support.attachments.MultipartMessageSupport;
-import com.eviware.soapui.impl.wsdl.support.CompressionSupport;
-import com.eviware.soapui.impl.wsdl.support.http.HttpClientSupport;
-import com.eviware.soapui.impl.wsdl.support.soap.SoapUtils;
-import com.eviware.soapui.impl.wsdl.support.soap.SoapVersion;
-import com.eviware.soapui.impl.wsdl.support.wss.IncomingWss;
-import com.eviware.soapui.model.iface.Attachment;
-import com.eviware.soapui.model.mock.MockRequest;
-import com.eviware.soapui.settings.WsdlSettings;
-import com.eviware.soapui.support.StringUtils;
-import com.eviware.soapui.support.Tools;
-import com.eviware.soapui.support.types.StringToStringMap;
-import com.eviware.soapui.support.types.StringToStringsMap;
-import com.eviware.soapui.support.xml.XmlUtils;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Vector;
 
 /**
  * Request-class created when receiving an external request to a WsdlMockService
@@ -69,60 +50,8 @@ public class WsdlMockRequest extends AbstractMockRequest {
 
         super(request, response, context);
 
-        if ("POST".equals(request.getMethod())) {
-            initPostRequest(request, context);
-        }
     }
 
-    protected void initPostRequest(HttpServletRequest request, WsdlMockRunContext context) throws Exception {
-        String contentType = request.getContentType();
-
-        if (contentType != null && contentType.toUpperCase().startsWith("MULTIPART")) {
-            readMultipartRequest(request);
-            MultipartMessageSupport multipartMessageSupport = getMultipartMessageSupport();
-            if (multipartMessageSupport != null && multipartMessageSupport.getRootPart() != null) {
-                contentType = multipartMessageSupport.getRootPart().getContentType();
-            }
-        } else {
-            String requestContent = readRequestContent(request);
-            super.setRequestContent(requestContent);
-
-            WsdlMockService mockService = (WsdlMockService) context.getMockService();
-            if (StringUtils.hasContent(mockService.getIncomingWss())) {
-                IncomingWss incoming = ((WsdlProject) mockService.getProject()).getWssContainer()
-                        .getIncomingWssByName(mockService.getIncomingWss());
-                if (incoming != null) {
-                    Document dom = XmlUtils.parseXml(requestContent);
-                    try {
-                        wssResult = incoming.processIncoming(dom, context);
-                        if (wssResult != null && wssResult.size() > 0) {
-                            StringWriter writer = new StringWriter();
-                            XmlUtils.serialize(dom, writer);
-                            setActualRequestContent(requestContent);
-                            super.setRequestContent(writer.toString());
-                        }
-                    } catch (Exception e) {
-                        if (wssResult == null) {
-                            wssResult = new Vector<Object>();
-                        }
-                        wssResult.add(e);
-                    }
-                }
-            }
-        }
-
-        try {
-            soapVersion = SoapUtils.deduceSoapVersion(contentType, getRequestXmlObject());
-        } catch (Exception e) {
-            // ignore non xml requests
-        }
-
-        if (soapVersion == null) {
-            soapVersion = SoapVersion.Soap11;
-        }
-
-        soapAction = SoapUtils.getSoapAction(soapVersion, getRequestHeaders());
-    }
 
     public SoapVersion getSoapVersion() {
         return soapVersion;
@@ -136,80 +65,6 @@ public class WsdlMockRequest extends AbstractMockRequest {
         return wssResult;
     }
 
-    private void readMultipartRequest(HttpServletRequest request) throws MessagingException {
-        StringToStringMap values = StringToStringMap.fromHttpHeader(request.getContentType());
-        MockRequestDataSource mockRequestDataSource = new MockRequestDataSource(request);
-        setMockRequestDataSource(mockRequestDataSource);
-        Settings settings = getRequestContext().getMockService().getSettings();
-        boolean isPrettyPrint = settings.getBoolean(WsdlSettings.PRETTY_PRINT_RESPONSE_MESSAGES);
-        MultipartMessageSupport mmSupport = new MultipartMessageSupport(mockRequestDataSource, values.get("start"), null, true, isPrettyPrint);
-        setMultipartMessageSupport(mmSupport);
-    }
-
-    private String readRequestContent(HttpServletRequest request) throws Exception {
-        String messageContent = null;
-        String encoding = request.getCharacterEncoding();
-        if (encoding != null) {
-            encoding = StringUtils.unquote(encoding);
-        }
-
-        ServletInputStream is = request.getInputStream();
-        if (is.markSupported() && request.getContentLength() > 0) {
-            is.mark(request.getContentLength());
-        }
-
-        ByteArrayOutputStream out = Tools.readAll(is, Tools.READ_ALL);
-        byte[] data = out.toByteArray();
-
-        if (is.markSupported() && request.getContentLength() > 0) {
-            try {
-                is.reset();
-            } catch (IOException e) {
-                SoapUI.logError(e);
-            }
-        }
-
-        // decompress
-        String compressionAlg = HttpClientSupport.getCompressionType(request.getContentType(),
-                getRequestHeaders().get("Content-Encoding", (String) null));
-
-        if (compressionAlg != null) {
-            try {
-                data = CompressionSupport.decompress(compressionAlg, data);
-            } catch (Exception e) {
-                IOException ioe = new IOException("Decompression of response failed");
-                ioe.initCause(e);
-                throw ioe;
-            }
-        }
-
-        int contentOffset = 0;
-
-        String contentType = request.getContentType();
-        if (contentType != null && data.length > 0) {
-            if (contentType.toLowerCase().endsWith("xml")) {
-                if (data.length > 3 && data[0] == (byte) 239 && data[1] == (byte) 187 && data[2] == (byte) 191) {
-                    encoding = "UTF-8";
-                    contentOffset = 3;
-                }
-            }
-
-            encoding = StringUtils.unquote(encoding);
-
-            messageContent = encoding == null ? new String(data) : new String(data, contentOffset,
-                    (int) (data.length - contentOffset), encoding);
-        }
-
-        if (encoding == null) {
-            encoding = "UTF-8";
-        }
-
-        if (messageContent == null) {
-            messageContent = new String(data, encoding);
-        }
-
-        return messageContent;
-    }
 
     public void setRequestContent(String requestContent) {
         super.setRequestContent(requestContent);
@@ -238,5 +93,52 @@ public class WsdlMockRequest extends AbstractMockRequest {
     public void setSoapAction(String soapAction) {
         this.soapAction = soapAction;
     }
+
+    protected void initProtocolSpecificPostContent(WsdlMockRunContext context, String contentType) throws IOException {
+        if (!isMultiPart(contentType)) {
+            addWSSResult(context, getRequestContent());
+        }
+        initSoapVersion(contentType);
+
+        soapAction = SoapUtils.getSoapAction(soapVersion, getRequestHeaders());
+    }
+
+    private void addWSSResult(WsdlMockRunContext context, String requestContent) throws IOException {
+        WsdlMockService mockService = (WsdlMockService) context.getMockService();
+        if (StringUtils.hasContent(mockService.getIncomingWss())) {
+            IncomingWss incoming = ((WsdlProject) mockService.getProject()).getWssContainer()
+                    .getIncomingWssByName(mockService.getIncomingWss());
+            if (incoming != null) {
+                Document dom = XmlUtils.parseXml(requestContent);
+                try {
+                    wssResult = incoming.processIncoming(dom, context);
+                    if (wssResult != null && wssResult.size() > 0) {
+                        StringWriter writer = new StringWriter();
+                        XmlUtils.serialize(dom, writer);
+                        setActualRequestContent(requestContent);
+                        super.setRequestContent(writer.toString());
+                    }
+                } catch (Exception e) {
+                    if (wssResult == null) {
+                        wssResult = new Vector<Object>();
+                    }
+                    wssResult.add(e);
+                }
+            }
+        }
+    }
+
+    private void initSoapVersion(String contentType) {
+        try {
+            soapVersion = SoapUtils.deduceSoapVersion(contentType, getRequestXmlObject());
+        } catch (Exception e) {
+            // ignore non xml requests
+        }
+
+        if (soapVersion == null) {
+            soapVersion = SoapVersion.Soap11;
+        }
+    }
+
 
 }

soapui/src/main/java/com/eviware/soapui/impl/rest/mock/RestMockDispatcher.java
@@ -38,10 +38,11 @@ public class RestMockDispatcher extends AbstractMockDispatcher {
 
     @Override
     public MockResult dispatchRequest(HttpServletRequest request, HttpServletResponse response) {
-        RestMockRequest restMockRequest = new RestMockRequest(request, response, mockContext);
 
+        RestMockRequest restMockRequest = null;
         Object result = null;
         try {
+            restMockRequest = new RestMockRequest(request, response, mockContext);
             result = mockService.runOnRequestScript(mockContext, restMockRequest);
 
             if (!(result instanceof MockResult)) {

soapui/src/main/java/com/eviware/soapui/impl/rest/mock/RestMockRequest.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RestMockRequest extends AbstractMockRequest {
-    public RestMockRequest(HttpServletRequest request, HttpServletResponse response, WsdlMockRunContext context) {
+    public RestMockRequest(HttpServletRequest request, HttpServletResponse response, WsdlMockRunContext context) throws Exception {
         super(request, response, context);
     }
 

soapui/src/main/java/com/eviware/soapui/impl/support/AbstractMockRequest.java
@@ -15,21 +15,32 @@
 */
 package com.eviware.soapui.impl.support;
 
+import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.impl.rest.RestRequestInterface;
 import com.eviware.soapui.impl.wsdl.WsdlOperation;
 import com.eviware.soapui.impl.wsdl.mock.WsdlMockRunContext;
 import com.eviware.soapui.impl.wsdl.submit.transports.http.support.attachments.MockRequestDataSource;
 import com.eviware.soapui.impl.wsdl.submit.transports.http.support.attachments.MultipartMessageSupport;
+import com.eviware.soapui.impl.wsdl.support.CompressionSupport;
+import com.eviware.soapui.impl.wsdl.support.http.HttpClientSupport;
 import com.eviware.soapui.model.iface.Attachment;
 import com.eviware.soapui.model.mock.MockRequest;
+import com.eviware.soapui.model.settings.Settings;
+import com.eviware.soapui.settings.WsdlSettings;
 import com.eviware.soapui.support.StringUtils;
+import com.eviware.soapui.support.Tools;
+import com.eviware.soapui.support.types.StringToStringMap;
 import com.eviware.soapui.support.types.StringToStringsMap;
 import com.eviware.soapui.support.xml.XmlUtils;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 
+import javax.mail.MessagingException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Enumeration;
 
 public abstract class AbstractMockRequest implements MockRequest {
@@ -47,7 +58,7 @@ public abstract class AbstractMockRequest implements MockRequest {
     private boolean responseMessage;
     private XmlObject requestXmlObject;
 
-    public AbstractMockRequest(HttpServletRequest request, HttpServletResponse response, WsdlMockRunContext context) {
+    public AbstractMockRequest(HttpServletRequest request, HttpServletResponse response, WsdlMockRunContext context) throws Exception {
         this.request = request;
         this.response = response;
         this.context = context;
@@ -76,7 +87,118 @@ public abstract class AbstractMockRequest implements MockRequest {
         if (path == null) {
             path = "";
         }
+
+        if ("POST".equals(request.getMethod())) {
+            initPostRequest(request, context);
+        }
+
     }
+
+    protected void initPostRequest(HttpServletRequest request, WsdlMockRunContext context) throws Exception {
+        String contentType = request.getContentType();
+
+        if (isMultiPart(contentType)) {
+            readMultipartRequest(request);
+            contentType = getMultiPartContentType(contentType);
+        } else {
+            String requestContent = readRequestContent(request);
+            setRequestContent(requestContent);
+        }
+
+        initProtocolSpecificPostContent(context, contentType);
+    }
+
+    protected void initProtocolSpecificPostContent(WsdlMockRunContext context, String contentType) throws IOException {
+        //Implemented by sub classes
+    }
+
+    protected boolean isMultiPart(String contentType) {
+        return contentType != null && contentType.toUpperCase().startsWith("MULTIPART");
+    }
+
+    private String getMultiPartContentType(String contentType) {
+        MultipartMessageSupport multipartMessageSupport = getMultipartMessageSupport();
+        if (multipartMessageSupport != null && multipartMessageSupport.getRootPart() != null) {
+            contentType = multipartMessageSupport.getRootPart().getContentType();
+        }
+        return contentType;
+    }
+
+    private void readMultipartRequest(HttpServletRequest request) throws MessagingException {
+        StringToStringMap values = StringToStringMap.fromHttpHeader(request.getContentType());
+        MockRequestDataSource mockRequestDataSource = new MockRequestDataSource(request);
+        setMockRequestDataSource(mockRequestDataSource);
+        Settings settings = getRequestContext().getMockService().getSettings();
+        boolean isPrettyPrint = settings.getBoolean(WsdlSettings.PRETTY_PRINT_RESPONSE_MESSAGES);
+        MultipartMessageSupport mmSupport = new MultipartMessageSupport(mockRequestDataSource, values.get("start"), null, true, isPrettyPrint);
+        setMultipartMessageSupport(mmSupport);
+    }
+
+    private String readRequestContent(HttpServletRequest request) throws Exception {
+        String messageContent = null;
+        String encoding = request.getCharacterEncoding();
+        if (encoding != null) {
+            encoding = StringUtils.unquote(encoding);
+        }
+
+        ServletInputStream is = request.getInputStream();
+        if (is.markSupported() && request.getContentLength() > 0) {
+            is.mark(request.getContentLength());
+        }
+
+        ByteArrayOutputStream out = Tools.readAll(is, Tools.READ_ALL);
+        byte[] data = out.toByteArray();
+
+        if (is.markSupported() && request.getContentLength() > 0) {
+            try {
+                is.reset();
+            } catch (IOException e) {
+                SoapUI.logError(e);
+            }
+        }
+
+        // decompress
+        String compressionAlg = HttpClientSupport.getCompressionType(request.getContentType(),
+                getRequestHeaders().get("Content-Encoding", (String) null));
+
+        if (compressionAlg != null) {
+            try {
+                data = CompressionSupport.decompress(compressionAlg, data);
+            } catch (Exception e) {
+                IOException ioe = new IOException("Decompression of response failed");
+                ioe.initCause(e);
+                throw ioe;
+            }
+        }
+
+        int contentOffset = 0;
+
+        String contentType = request.getContentType();
+        if (contentType != null && data.length > 0) {
+            if (contentType.toLowerCase().endsWith("xml")) {
+                if (data.length > 3 && data[0] == (byte) 239 && data[1] == (byte) 187 && data[2] == (byte) 191) {
+                    encoding = "UTF-8";
+                    contentOffset = 3;
+                }
+            }
+
+            encoding = StringUtils.unquote(encoding);
+
+            messageContent = encoding == null ? new String(data) : new String(data, contentOffset,
+                    (int) (data.length - contentOffset), encoding);
+        }
+
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
+
+        if (messageContent == null) {
+            messageContent = new String(data, encoding);
+        }
+
+        return messageContent;
+    }
+
 
     public String getProtocol() {
         return protocol;

soapui/src/test/java/com/eviware/soapui/impl/rest/mock/RestMockResponseWriterTest.java
@@ -42,7 +42,7 @@ public class RestMockResponseWriterTest {
     private HttpServletResponse servletResponse;
 
     @Before
-    public void setUp() throws IOException, SoapUIException {
+    public void setUp() throws Exception {
         servletRequest = mockHttpServletRequest();
         servletResponse = mockHttpServletResponse();
         mockResponse = ModelItemFactory.makeRestResponse();
@@ -75,12 +75,12 @@ public class RestMockResponseWriterTest {
         verify(servletResponse).setStatus(HttpStatus.SC_CONFLICT);
     }
 
-    public MockResult createMockResult() throws IOException {
+    public MockResult createMockResult() throws Exception {
         WsdlMockRunContext runContext = mock(WsdlMockRunContext.class);
         return new RestMockResult(createMockRequest(runContext));
     }
 
-    public RestMockRequest createMockRequest(WsdlMockRunContext runContext) throws IOException {
+    public RestMockRequest createMockRequest(WsdlMockRunContext runContext) throws Exception {
         return new RestMockRequest(servletRequest, servletResponse, runContext);
     }
 

commit 009dc80b06a4503296af7cd37db1d72410bd5f6d

Fix dispatching of encrypted SOAP body in mock services.
DOM version of request was not updated due to a bug introduced during
refactoring in commit 8f921d6983235e268513bed9837bc5d56d90941c. This
caused findOperationForRequest to consider encrypted body for SOAP
operation determination.

The following exception in error log indicated the problem:

  ```
  ERROR:com.eviware.soapui.impl.wsdl.mock.DispatchException: Missing operation for soapAction [urn:xxx:yyy/zzz/op] and body element [{http://www.w3.org/2001/04/xmlenc#}EncryptedData] with SOAP Version [SOAP 1.1]
    com.eviware.soapui.impl.wsdl.mock.DispatchException: Missing operation for soapAction [urn:xxx:yyy/zzz/op] and body element [{http://www.w3.org/2001/04/xmlenc#}EncryptedData] with SOAP Version [SOAP 1.1]
    at com.eviware.soapui.impl.wsdl.support.soap.SoapUtils.findOperationForRequest(SoapUtils.java:328)
    at com.eviware.soapui.impl.wsdl.mock.WsdlMockDispatcher.dispatchPostRequest(WsdlMockDispatcher.java:192)
    at com.eviware.soapui.impl.wsdl.mock.WsdlMockDispatcher.dispatchRequest(WsdlMockDispatcher.java:114)
    at com.eviware.soapui.impl.wsdl.mock.WsdlMockRunner.dispatchRequest(WsdlMockRunner.java:144)
    at com.eviware.soapui.monitor.JettyMockEngine$ServerHandler.handle(JettyMockEngine.java:604)
    at org.mortbay.jetty.handler.HandlerCollection.handle(HandlerCollection.java:114)
    at org.mortbay.jetty.handler.HandlerWrapper.handle(HandlerWrapper.java:152)
    at org.mortbay.jetty.Server.handle(Server.java:326)
    at org.mortbay.jetty.HttpConnection.handleRequest(HttpConnection.java:542)
    at org.mortbay.jetty.HttpConnection$RequestHandler.content(HttpConnection.java:945)
    at org.mortbay.jetty.HttpParser.parseNext(HttpParser.java:843)
    at org.mortbay.jetty.HttpParser.parseAvailable(HttpParser.java:218)
    at org.mortbay.jetty.HttpConnection.handle(HttpConnection.java:404)
    at org.mortbay.io.nio.SelectChannelEndPoint.run(SelectChannelEndPoint.java:410)

soapui/src/main/java/com/eviware/soapui/impl/wsdl/mock/WsdlMockRequest.java
@@ -93,6 +93,7 @@ public class WsdlMockRequest extends AbstractMockRequest {
         this.soapAction = soapAction;
     }
 
+    @Override
     protected void initProtocolSpecificPostContent(WsdlMockRunContext context, String contentType) throws IOException {
         if (!isMultiPart(contentType)) {
             addWSSResult(context, getRequestContent());
@@ -115,7 +116,7 @@ public class WsdlMockRequest extends AbstractMockRequest {
                         StringWriter writer = new StringWriter();
                         XmlUtils.serialize(dom, writer);
                         setActualRequestContent(requestContent);
-                        super.setRequestContent(writer.toString());
+                        setRequestContent(writer.toString());
                     }
                 } catch (Exception e) {
                     if (wssResult == null) {