5830d038b8ac70e3d1500acdf8f615e749a9b2bc
Modestas Vainius <modestas@vainius.eu>
2019-01-16 19:57:36 +0200

    Refactor bootstrap sh scripts to use common base
    This way all startup scripts contain consistent code. Before they were
    basically the same copy & pasted code with very minor changes (with
    exception of soapui.sh which had slightly more differences). But even
    they could be put into reusable base script.
    
    Base script is called baserunner.sh and it is sourced from each
    bootstrap script. Improvements in comparison to prior code include:
    
    - JRE in SOAPUI_HOME support (1st priority) (previously only in dist_standalone);
    - JAVA_HOME support (2nd priority) (not in prior dist neither dist_standalone);
    - JAVA_OPTS from shell environment is passed to java process as well;
    - add symlink detection logic from soapui.sh to all *.sh scripts;
    - java process is exec'ed;
    - in a few place more modern shell syntax is used;
    - groovy.source.encoding=iso-8859-1 is not passed to JAVA_OPTS (used to
      be passed in dist_standalone but not in original dist).
    
    Tested on Debian GNU/Linux

--- soapui-installer/src/dist/bin/toolrunner.sh
+++ soapui-installer/src/dist/bin/toolrunner.sh
@@ -1,70 +1,23 @@
 #!/bin/sh
 ### ====================================================================== ###
 ##                                                                          ##
-##  SoapUI Pro ToolRunner Bootstrap Script                                      ##
+##  SoapUI ToolRunner Bootstrap Script                                      ##
 ##                                                                          ##
 ### ====================================================================== ###
 
 ### $Id$ ###
-
-DIRNAME=`dirname $0`
-
-# OS specific support (must be 'true' or 'false').
-cygwin=false;
-case "`uname`" in
-    CYGWIN*)
-        cygwin=true
-        ;;
-esac
-
-# Setup SOAPUI_HOME
-if [ -d $SOAPUI_HOME ]
+EXECUTABLE=`dirname "$0"`/`basename "$0"`
+LS_LD=`ls -ld "${EXECUTABLE}"`
+SYM_LINK_INDICATOR="->"
+if [ "$LS_LD" != "${LS_LD%$SYM_LINK_INDICATOR*}" ]
 then
-    # get the full path (without any relative bits)
-    SOAPUI_HOME=`cd $DIRNAME/..; pwd`
+  EXECUTABLE=`ls -ld "${EXECUTABLE}" | sed -e 's|.*-> ||' -e 's|.* ${EXECUTABLE}|${EXECUTABLE}|'`
+  case "$EXECUTABLE" in
+    /*);;
+    *)EXECUTABLE=`dirname $0`/$EXECUTABLE
+  esac
 fi
+DIRNAME=`dirname $EXECUTABLE`
 
-export SOAPUI_HOME
-
-if [ -f "$SOAPUI_HOME/jre/bin/java" ]
-then
-  JAVA=$SOAPUI_HOME/jre/bin/java
-else
-    if [ -f "$SOAPUI_HOME/../../PlugIns/jre.bundle/Contents/Home/jre/bin/java" ]
-    then
-        JAVA=$SOAPUI_HOME/../../PlugIns/jre.bundle/Contents/Home/jre/bin/java
-    else
-        JAVA=java
-    fi
-fi
-
-SOAPUI_CLASSPATH=$SOAPUI_HOME/bin/${project.src.artifactId}-${project.version}.jar:$SOAPUI_HOME/lib/*
-JFXRTPATH=`$JAVA -cp $SOAPUI_CLASSPATH com.eviware.soapui.tools.JfxrtLocator`
-SOAPUI_CLASSPATH=$JFXRTPATH:$SOAPUI_CLASSPATH
-
-export SOAPUI_CLASSPATH
-
-JAVA_OPTS="-Xms128m -Xmx1024m -Dsoapui.properties=soapui.properties -Dgroovy.source.encoding=iso-8859-1 -Dsoapui.home=$SOAPUI_HOME/bin"
-if [ $SOAPUI_HOME != "" ] 
-then
-    JAVA_OPTS="$JAVA_OPTS -Dsoapui.ext.libraries=$SOAPUI_HOME/bin/ext"
-    JAVA_OPTS="$JAVA_OPTS -Dsoapui.ext.listeners=$SOAPUI_HOME/bin/listeners"
-    JAVA_OPTS="$JAVA_OPTS -Dsoapui.ext.actions=$SOAPUI_HOME/bin/actions"
-fi
-
-export JAVA_OPTS
-
-# For Cygwin, switch paths to Windows format before running java
-if $cygwin
-then
-    SOAPUI_HOME=`cygpath --path --dos "$SOAPUI_HOME"`
-    SOAPUI_CLASSPATH=`cygpath --path --dos "$SOAPUI_CLASSPATH"`
-fi
-
-echo ================================
-echo =
-echo = SOAPUI_HOME = $SOAPUI_HOME
-echo =
-echo ================================
-
-$JAVA $JAVA_OPTS -cp $SOAPUI_CLASSPATH com.eviware.soapui.tools.SoapUIToolRunner "$@"
+MAIN_CLASS="com.eviware.soapui.tools.SoapUIToolRunner"
+. "$DIRNAME/baserunner.sh"
