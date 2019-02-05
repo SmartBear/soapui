709dcee940ea5057e20c4b21bbf50b477dc79683
ChristinaZelenko <christina.zelenko@smartbear.com>
2019-02-05 14:49:29 +0300

    SOAPUIOS-461 Added context menu to inputs and textareas

--- soapui/src/main/java/com/eviware/soapui/support/components/EnabledWebViewBasedBrowserComponent.java
+++ soapui/src/main/java/com/eviware/soapui/support/components/EnabledWebViewBasedBrowserComponent.java
@@ -81,15 +81,9 @@ class EnabledWebViewBasedBrowserComponent implements WebViewBasedBrowserComponen
 
     private JFXPanel browserPanel;
     private PopupStrategy popupStrategy;
-    private final boolean withContextMenu;
 
     EnabledWebViewBasedBrowserComponent(boolean addNavigationBar, PopupStrategy popupStrategy) {
-        this(addNavigationBar, false, popupStrategy);
-    }
-
-    public EnabledWebViewBasedBrowserComponent(boolean addNavigationBar, boolean withContextMenu, PopupStrategy popupStrategy) {
         this.popupStrategy = popupStrategy;
-        this.withContextMenu = withContextMenu;
         initializeWebView(addNavigationBar);
     }
 
@@ -373,7 +367,6 @@ class EnabledWebViewBasedBrowserComponent implements WebViewBasedBrowserComponen
             }
             browserPanel.setScene(createJfxScene());
             addKeyboardFocusManager(browserPanel);
-            webView.setContextMenuEnabled(withContextMenu);
         }
 
         private Scene createJfxScene() {
