14270dc6e7e38272cfecb485e26148bcbca6d72a
AlexanderYeremeyev <Alexander.Yeremeyev@smartbear.com>
2020-05-17 21:23:07 +0300

    extract navigatorTree from navigator

--- soapui/src/main/java/com/eviware/soapui/ui/navigator/Navigator.java
+++ soapui/src/main/java/com/eviware/soapui/ui/navigator/Navigator.java
@@ -117,50 +117,6 @@ public class Navigator extends JPanel {
         return toolbar;
     }
 
-    private static class NavigatorTree extends JTree implements Autoscroll {
-        public NavigatorTree(SoapUITreeModel treeModel) {
-            super(treeModel);
-        }
-
-        private static final int AUTOSCROLL_MARGIN = 12;
-
-        public void autoscroll(Point pt) {
-            // Figure out which row we�re on.
-            int nRow = getRowForLocation(pt.x, pt.y);
-
-            // If we are not on a row then ignore this autoscroll request
-            if (nRow < 0) {
-                return;
-            }
-
-            Rectangle raOuter = getBounds();
-            // Now decide if the row is at the top of the screen or at the
-            // bottom. We do this to make the previous row (or the next
-            // row) visible as appropriate. If we�re at the absolute top or
-            // bottom, just return the first or last row respectively.
-
-            nRow = (pt.y + raOuter.y <= AUTOSCROLL_MARGIN) // Is row at top of
-                    // screen?
-                    ? (nRow <= 0 ? 0 : nRow - 1) // Yes, scroll up one row
-                    : (nRow < getRowCount() - 1 ? nRow + 1 : nRow); // No,
-            // scroll
-            // down one
-            // row
-
-            scrollRowToVisible(nRow);
-        }
-
-        // Calculate the insets for the *JTREE*, not the viewport
-        // the tree is in. This makes it a bit messy.
-        public Insets getAutoscrollInsets() {
-            Rectangle raOuter = getBounds();
-            Rectangle raInner = getParent().getBounds();
-            return new Insets(raInner.y - raOuter.y + AUTOSCROLL_MARGIN, raInner.x - raOuter.x + AUTOSCROLL_MARGIN,
-                    raOuter.height - raInner.height - raInner.y + raOuter.y + AUTOSCROLL_MARGIN, raOuter.width
-                    - raInner.width - raInner.x + raOuter.x + AUTOSCROLL_MARGIN);
-        }
-    }
-
     public Project getCurrentProject() {
         TreePath path = mainTree.getSelectionPath();
         if (path == null) {
