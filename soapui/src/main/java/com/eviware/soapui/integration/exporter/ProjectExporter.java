d60051317e896e7769779ea2a26f6a2cde80cd29
Maciej Bartlodziej <maciej.bartlodziej@smartbear.com>
2023-08-25 16:16:25 +0200

    RIA-21268 optimized imports

--- soapui/src/main/java/com/eviware/soapui/integration/exporter/ProjectExporter.java
+++ soapui/src/main/java/com/eviware/soapui/integration/exporter/ProjectExporter.java
@@ -21,7 +21,6 @@ import com.eviware.soapui.impl.wsdl.WsdlProject;
 import com.eviware.soapui.impl.wsdl.support.ExternalDependency;
 import com.eviware.soapui.model.project.ProjectFactoryRegistry;
 import com.eviware.soapui.support.SoapUIException;
-import com.eviware.soapui.support.UISupport;
 import org.apache.commons.io.FileUtils;
 import org.apache.xmlbeans.XmlException;
 
@@ -29,7 +28,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
